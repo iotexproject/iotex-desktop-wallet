785fc9597db1fb513f1028c6bb348e8094b13288
Tian Pan <puncsky@gmail.com>
2018-10-24 15:38:06 -0700

    remove nkn integration

--- src/server/start-server.js
+++ src/server/start-server.js
@@ -6,7 +6,6 @@ import {setMiddleware} from './middleware';
 import {setServerRoutes} from './server-routes';
 import {setGateways} from './gateways/gateways';
 import {setService} from './service/service';
-import {setupNknRpc} from './nkn-rpc/nkn-rpc';
 
 export async function startServer() {
   const server = new Server();
@@ -15,7 +14,6 @@ export async function startServer() {
   setGateways(server);
   setService(server);
   setServerRoutes(server);
-  setupNknRpc(server);
 
   // eslint-disable-next-line no-process-env,no-undef
   const port = parseInt(process.env.PORT || config.get('server.port'), 10);
