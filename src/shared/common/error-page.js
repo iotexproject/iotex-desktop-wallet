ee3cded016e41d34b67c49d9cb148d17f9277764
Tian Pan <puncsky@gmail.com>
2019-01-02 22:37:40 -0800

    downgrade react-redux and fix tests

--- src/shared/common/error-page.js
+++ src/shared/common/error-page.js
@@ -3,12 +3,12 @@ import {PureComponent} from 'react';
 import Helmet from 'onefx/lib/react-helmet';
 import {styled} from 'onefx/lib/styletron-react';
 import {assetURL} from 'onefx/lib/asset-url';
+import {t} from 'onefx/lib/iso-i18n';
 import {fullOnPalm} from './styles/style-media';
 import {colors} from './styles/style-color';
 import {ContentPadding} from './styles/style-padding';
 import {Flex} from './flex';
 import {FOOTER_ABOVE} from './footer';
-import {t} from "onefx/lib/iso-i18n";
 
 type Props = {
   bar: string, title: string, info: string,
--- src/shared/common/not-found.js
+++ src/shared/common/not-found.js
@@ -1,27 +1,19 @@
 // @flow
-import {PureComponent} from 'react';
 import {t} from 'onefx/lib/iso-i18n';
 import {Route} from 'react-router';
 import {ErrorPage} from './error-page';
 
-export class NotFound extends PureComponent {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Status code={404}>
-        <ErrorPage bar={t('not_found.bar')} title={t('not_found.title')} info={t('not_found.info')}/>
-      </Status>
-    );
-  }
+export function NotFound() {
+  return (
+    <Status code={404}>
+      <ErrorPage bar={t('not_found.bar')} title={t('not_found.title')} info={t('not_found.info')}/>
+    </Status>
+  );
 }
 
 const Status = ({code, children}: any) => (
   <Route
-    render={({staticContext}) => {
+    render={({staticContext}: any) => {
       if (staticContext) {
         staticContext.status = code;
       }
