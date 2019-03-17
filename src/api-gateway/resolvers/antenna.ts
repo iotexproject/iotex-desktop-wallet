5634a9f84f87893578df68566e5d00a87b06fa7a
Tian Pan <puncsky@gmail.com>
2019-03-16 22:28:59 -0700

    add getAccount

--- src/api-gateway/resolvers/antenna.ts
+++ src/api-gateway/resolvers/antenna.ts
@@ -1,5 +1,5 @@
-import { Ctx, Query, Resolver, ResolverInterface } from "type-graphql";
-import { ChainMeta } from "./antenna-types";
+import { Arg, Ctx, Query, Resolver, ResolverInterface } from "type-graphql";
+import { ChainMeta, GetAccountResponse } from "./antenna-types";
 
 @Resolver(_ => ChainMeta)
 export class AntennaResolver implements ResolverInterface<() => ChainMeta> {
@@ -8,5 +8,15 @@ export class AntennaResolver implements ResolverInterface<() => ChainMeta> {
   public async chainMeta(@Ctx() { gateways }: any): Promise<ChainMeta> {
     const chainMeta = await gateways.antenna.getChainMeta({});
     return chainMeta.chainMeta;
+  }
+
+  @Query(_ => GetAccountResponse)
+  public async getAccount(
+    @Arg("address", _ => String, { description: "iotex address" })
+    address: string,
+    // tslint:disable-next-line:no-any
+    @Ctx() { gateways }: any
+  ): Promise<GetAccountResponse> {
+    return gateways.antenna.getAccount({ address });
   }
 }
