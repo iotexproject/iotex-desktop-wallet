bdcad5c1c1a61467d48aa7fe34ad154d10a4ccf7
hung le <hunglmtb@gmail.com>
2019-03-18 19:20:13 +0700

    porting function to resolve part 1

--- src/api-gateway/resolvers/antenna-types.ts
+++ src/api-gateway/resolvers/antenna-types.ts
@@ -40,3 +40,75 @@ export class GetAccountResponse {
   @Field(_ => AccountMeta)
   public accountMeta: AccountMeta;
 }
+
+@ObjectType({ description: "Properties of an blockMeta" })
+export class BlockMeta {
+  @Field(_ => String)
+  public hash: string;
+  @Field(_ => Int)
+  public height: number;
+  @Field(_ => Int)
+  public timestamp: number;
+  @Field(_ => Int)
+  public numActions: number;
+  @Field(_ => String)
+  public producerAddress: string;
+  @Field(_ => String)
+  public transferAmount: string;
+  @Field(_ => String)
+  public txRoot: string;
+  @Field(_ => String)
+  public receiptRoot: string;
+  @Field(_ => String)
+  public deltaStateDigest: string;
+}
+
+@ObjectType({ description: "Properties of an Log" })
+export class Log {
+  @Field(_ => String)
+  public address: string;
+  @Field(_ => String)
+  public topics: Array<number>;
+  @Field(_ => String)
+  public data: Array<number>;
+  @Field(_ => Int)
+  public blockNumber: number;
+  @Field(_ => String)
+  public txnHash: Array<number>;
+  @Field(_ => Int)
+  public index: number;
+}
+
+@ObjectType({ description: "Properties of an Receipt" })
+export class Receipt {
+  @Field(_ => String)
+  public returnValue: Array<number>;
+  @Field(_ => Int)
+  public status: number;
+  @Field(_ => Int)
+  public actHash: Array<number>;
+  @Field(_ => Int)
+  public gasConsumed: number;
+  @Field(_ => String)
+  public contractAddress: string;
+  @Field(_ => [Log])
+  public logs: Array<Log>;
+}
+
+@ObjectType()
+export class GetBlockMetasResponse {
+  @Field(_ => [BlockMeta])
+  public blkMetas: Array<BlockMeta>;
+}
+
+@ObjectType()
+export class SuggestGasPriceResponse {
+  @Field(_ => Int)
+  public gasPrice: number;
+}
+
+@ObjectType()
+export class GetReceiptByActionResponse {
+  @Field(_ => Receipt)
+  public receipt: Receipt;
+}
