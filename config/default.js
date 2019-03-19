b61fee4a6198229b2750a085f7dff526292395cc
Tian Pan <puncsky@gmail.com>
2019-03-18 19:23:35 -0700

    update testnet addr

--- config/default.js
+++ config/default.js
@@ -27,7 +27,7 @@ module.exports = {
       enabled: true,
       level: "debug"
     },
-    iotexAntenna: process.env.IOTEX_ANTENNA || "35.203.158.71:14014"
+    iotexAntenna: process.env.IOTEX_ANTENNA || "35.230.103.170:10000"
   },
   analytics: {
     googleTid: "TODO: replace with your googleTid"
