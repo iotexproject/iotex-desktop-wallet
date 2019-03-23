0f025811b5c7dc4bbe348d14ca14a5fb5c996086
Hung Le <hunglmtb@gmail.com>
2019-03-23 14:08:09 +0700

    add /actions (#126)

--- src/shared/address-details/action-table.tsx
+++ src/shared/address-details/action-table.tsx
@@ -68,6 +68,7 @@ export function ActionTable({ address }: { address: string }): JSX.Element {
         return (
           <SpinPreloader spinning={loading}>
             <Table columns={getActionColumns()} dataSource={actions} />
+
             <pre>{JSON.stringify(data, null, 2)}</pre>
           </SpinPreloader>
         );
