adbd3df40c9b24b8ae4214bb919b9c32c9b746af
yuyuko <ncysatnaf@gmail.com>
2019-03-23 13:02:25 +0800

    avoid last page empty

--- src/shared/block/blocks.tsx
+++ src/shared/block/blocks.tsx
@@ -49,7 +49,7 @@ function getBlockIndexRange(
 ): { start: number; count: number } {
   const start = endHeight - currentPage * PAGE_SIZE;
   return {
-    start,
+    start: start < 0 ? 0 : start,
     count: PAGE_SIZE
   };
 }
