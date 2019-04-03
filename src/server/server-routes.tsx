9c2772471db4b9672c4b884c9ddf42f64f5f0971
yuyuko <ncysatnaf@gmail.com>
2019-04-03 14:16:57 +0800

    add coming soon page

--- src/server/server-routes.tsx
+++ src/server/server-routes.tsx
@@ -14,15 +14,11 @@ export function setServerRoutes(server: Server): void {
     ctx.body = "OK";
   });
 
-  server.get("wallet", "/wallet", (ctx: koa.Context) => {
-    ctx.redirect("/");
-  });
-
   setApiGateway(server);
 
   server.get(
     "SPA",
-    "(/|/address/.*|/blocks|/block/.*|/actions|/action/.*)",
+    "(/|/address/.*|/blocks|/block/.*|/actions|/action/.*|/wallet)",
     async (ctx: koa.Context) => {
       ctx.setState("base.blah", "this is a sample initial state");
       ctx.body = await apolloSSR(ctx, server.config.apiGatewayUrl, {
