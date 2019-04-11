6b2aa4f2f324801470bf9ad6f5eca5e5bd6fd1b9
koishi <ncysatnaf@gmail.com>
2019-04-11 15:39:57 +0800

    P2 1.0 Transfer Token

--- src/shared/wallet/wallet.tsx
+++ src/shared/wallet/wallet.tsx
@@ -1,12 +1,16 @@
 import Col from "antd/lib/grid/col";
 import Row from "antd/lib/grid/row";
+import Tabs from "antd/lib/tabs";
 import { Account } from "iotex-antenna/lib/account/account";
+// @ts-ignore
+import { t } from "onefx/lib/iso-i18n";
 import React, { Component } from "react";
 import { AccountMeta } from "../../api-gateway/resolvers/antenna-types";
 import { ContentPadding } from "../common/styles/style-padding";
 import AccountSection from "./account-section";
 import { getAntenna } from "./get-antenna";
 import NewWallet from "./new-wallet";
+import Transfer from "./transfer/transfer";
 import UnlockWallet from "./unlock-wallet";
 
 export interface State {
@@ -41,6 +45,44 @@ export default class Wallet extends Component<Props, State> {
     }
   };
 
+  public tabs = ({
+    wallet,
+    address
+  }: {
+    wallet: Account;
+    address: AccountMeta;
+  }) => {
+    return (
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane
+          key="1"
+          tab={t("wallet.tab.transfer", { token: t("account.testnet.token") })}
+        >
+          <Transfer wallet={wallet} address={address} />
+        </Tabs.TabPane>
+        <Tabs.TabPane key="2" tab={t("wallet.tab.vote")}>
+          //TODO
+        </Tabs.TabPane>
+        <Tabs.TabPane key="3" tab={t("wallet.tab.contract")}>
+          //TODO
+        </Tabs.TabPane>
+      </Tabs>
+    );
+  };
+
+  public noWallet = () => {
+    const { createNew } = this.state;
+    return createNew ? (
+      <NewWallet setWallet={this.setWallet} />
+    ) : (
+      <UnlockWallet
+        setWallet={this.setWallet}
+        setCreateNew={() => this.setState({ createNew: true })}
+        chainId={1}
+      />
+    );
+  };
+
   public render(): JSX.Element {
     const { createNew, wallet, address } = this.state;
     return (
@@ -48,14 +90,8 @@ export default class Wallet extends Component<Props, State> {
         <div style={{ margin: "48px" }} />
         <Row>
           <Col md={16}>
-            {createNew && <NewWallet setWallet={this.setWallet} />}
-            {!createNew && (
-              <UnlockWallet
-                setWallet={this.setWallet}
-                setCreateNew={() => this.setState({ createNew: true })}
-                chainId={1}
-              />
-            )}
+            {wallet && address && this.tabs({ wallet, address })}
+            {!wallet && this.noWallet()}
           </Col>
           <Col md={6} push={2}>
             <AccountSection
