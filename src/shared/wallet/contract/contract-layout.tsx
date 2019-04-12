b179dfe481f7acbf151359cdbf51ea424b815e86
koishi <ncysatnaf@gmail.com>
2019-04-12 17:36:12 +0800

    clearn code

--- src/shared/wallet/contract/contract-layout.tsx
+++ src/shared/wallet/contract/contract-layout.tsx
@@ -1,17 +1,8 @@
-// import Col from "antd/lib/grid/col";
-// import Row from "antd/lib/grid/row";
-// import Icon from "antd/lib/icon";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 // @ts-ignore
 import Helmet from "onefx/lib/react-helmet";
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import { PageTitle } from "../../common/page-title";
-// import { ContentPadding } from "../../common/styles/style-padding";
-// import AccountSection from "../account-section";
-
-// const ACTION_LINK = "/dev/wallet/smart-contract";
 
 export interface Props {
   title: string;
@@ -28,18 +19,6 @@ export class ContractLayout extends Component<Props, State> {
         <Helmet title={`IoTeX ${title}`} />
         <div style={{ margin: "24px" }} />
         {children}
-        {/* <Row key={0}>
-          <Col md={16}>
-            <Link to={ACTION_LINK}>{t("wallet.contract.back")}</Link>
-            <PageTitle>
-              <Icon type="profile" /> {title}
-            </PageTitle>
-            
-          </Col>
-          <Col md={6} push={2}>
-            <AccountSection />
-          </Col>
-        </Row> */}
       </div>
     );
   }
