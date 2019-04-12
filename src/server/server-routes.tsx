0469b49527c3aa17b63692753a7288a3263671e5
Hung Le <hunglmtb@gmail.com>
2019-04-12 14:24:33 +0700

    Contract deploy interact (#256)
    * fix choose function title format
    
    * add contract deloy and interact UI
    
    * fix run test version react
    
    * update icon and color

--- src/server/server-routes.tsx
+++ src/server/server-routes.tsx
@@ -18,7 +18,7 @@ export function setServerRoutes(server: Server): void {
 
   server.get(
     "SPA",
-    "(/|/address/.*|/blocks|/block/.*|/actions|/action/.*|/wallet|/dev/.*)",
+    "(/|/address/.*|/blocks|/block/.*|/actions|/action/.*|/wallet|/dev/.*|/dev/wallet/.*)",
     async (ctx: koa.Context) => {
       ctx.setState("base.blah", "this is a sample initial state");
       ctx.body = await apolloSSR(ctx, server.config.apiGatewayUrl, {
--- src/shared/contract/cards.tsx
+++ src/shared/contract/cards.tsx
@@ -1,11 +1,16 @@
 // @ts-ignore
-import { Avatar, Card, Icon } from "antd";
+import { Avatar, Card, Form, Icon, Input, InputNumber } from "antd";
+import { WrappedFormUtils } from "antd/lib/form/Form";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
+// @ts-ignore
+import { styled } from "onefx/lib/styletron-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Flex } from "../common/flex";
+import { formItemLayout } from "../common/form-item-layout";
 import { colors } from "../common/styles/style-color";
+const { TextArea } = Input;
 
 type CardFunctionProps = {
   title: string;
@@ -23,6 +28,16 @@ const cardStyle = {
   boxShadow: "0 2px 8px 0 rgba(0, 0, 0, 0.16)",
   margin: "10px 0"
 };
+
+export const inputStyle = {
+  width: "100%",
+  background: colors.black10,
+  border: "none"
+};
+
+export const FormItemLabel = styled("label", {
+  fontWeight: "bold"
+});
 
 export const CardFunction = ({
   title,
@@ -52,3 +67,69 @@ export const CardFunction = ({
     </Flex>
   </Card>
 );
+
+export function AbiFormInputItem(form: WrappedFormUtils): JSX.Element {
+  const { getFieldDecorator } = form;
+  return (
+    <Form.Item
+      {...formItemLayout}
+      label={<FormItemLabel>{t("wallet.input.abi")}</FormItemLabel>}
+    >
+      {getFieldDecorator("abi", {
+        initialValue: "",
+        rules: []
+      })(
+        <TextArea
+          rows={4}
+          style={inputStyle}
+          placeholder={t("wallet.placeholder.abi")}
+        />
+      )}
+    </Form.Item>
+  );
+}
+
+export function NonceFormInputItem(form: WrappedFormUtils): JSX.Element {
+  const { getFieldDecorator } = form;
+  return (
+    <Form.Item
+      {...formItemLayout}
+      label={<FormItemLabel>{t("wallet.input.nonce")}</FormItemLabel>}
+    >
+      {getFieldDecorator("nonce", {
+        initialValue: "",
+        rules: []
+      })(<InputNumber size="large" step={1} min={1} style={inputStyle} />)}
+    </Form.Item>
+  );
+}
+
+export function GasPriceFormInputItem(form: WrappedFormUtils): JSX.Element {
+  const { getFieldDecorator } = form;
+  return (
+    <Form.Item
+      {...formItemLayout}
+      label={<FormItemLabel>{t("wallet.input.gasPrice")}</FormItemLabel>}
+    >
+      {getFieldDecorator("gasPrice", {
+        initialValue: "",
+        rules: []
+      })(<InputNumber size="large" step={10} min={0} style={inputStyle} />)}
+    </Form.Item>
+  );
+}
+
+export function GasLimitFormInputItem(form: WrappedFormUtils): JSX.Element {
+  const { getFieldDecorator } = form;
+  return (
+    <Form.Item
+      {...formItemLayout}
+      label={<FormItemLabel>{t("wallet.input.gasLimit")}</FormItemLabel>}
+    >
+      {getFieldDecorator("gasLimit", {
+        initialValue: "",
+        rules: []
+      })(<InputNumber size="large" step={10} min={0} style={inputStyle} />)}
+    </Form.Item>
+  );
+}
