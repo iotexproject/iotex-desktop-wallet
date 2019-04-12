ad643889cd400597895f8ca81db793b7f2e7dd77
Tian Pan <puncsky@gmail.com>
2019-04-12 01:36:38 -0700

    add deploy to storyboard

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,13 +6,30 @@ import { linkTo } from "@storybook/addon-links";
 import { Welcome } from "@storybook/react/demo";
 import { NotFound } from "../src/shared/common/not-found";
 import { TestRoot } from "../src/shared/common/test-root";
+import WriteContractModal from "../src/shared/wallet/write-contract-modal";
+import { Deploy } from "../src/shared/wallet/contract/deploy";
 
-storiesOf("Welcome", module).add("to Storybook", () => (
-  <Welcome showApp={linkTo("Button")} />
-));
+storiesOf("Welcome", module)
+  .add("to Storybook", () => <Welcome showApp={linkTo("Button")} />)
+  .add("WriteContractModal", () => (
+    <TestRoot>
+      <WriteContractModal
+        showModal={true}
+        generateTransaction={() => undefined}
+        networkAddress={"addddddreeeesss"}
+        amount={"10000"}
+      />
+    </TestRoot>
+  ));
 
 storiesOf("Pages", module).add("NotFound", () => (
   <TestRoot>
     <NotFound />
+  </TestRoot>
+));
+
+storiesOf("Smart Contract", module).add("Deploy", () => (
+  <TestRoot>
+    <Deploy />
   </TestRoot>
 ));
