eb731159bceb6c2e231db5b3ead39a3d1092d8f4
nmhung1210 <nmhung1210@gmail.com>
2019-05-07 19:25:51 +0700

    Implement save the keystore feature

---
+++ src/shared/common/xconf.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+import window from "global/window";
+
+// tslint:disable: no-any
+const tmpdata: { [index: string]: any } = {};
+
+// The xconf is a mockup for user configurations with electron.
+export const xconf: {
+  getConf(name: string, defaultvalue?: any): any;
+  setConf(name: string, value: any): boolean;
+} = window.xconf || {
+  getConf: (name: string, defaultvalue?: any): any => {
+    return tmpdata[name] || defaultvalue;
+  },
+  setConf: (name: string, value: any): boolean => {
+    tmpdata[name] = value;
+    return true;
+  }
+};
--- src/shared/wallet/unlock-by-keystore-file.tsx
+++ src/shared/wallet/unlock-by-keystore-file.tsx
@@ -1,22 +1,22 @@
 import Button from "antd/lib/button";
+import Dropdown from "antd/lib/dropdown";
 import Form, { FormComponentProps, WrappedFormUtils } from "antd/lib/form/Form";
 import Icon from "antd/lib/icon";
+import Menu from "antd/lib/menu";
 import notification from "antd/lib/notification";
+import Tag from "antd/lib/tag";
 import Upload from "antd/lib/upload";
 import { RcFile } from "antd/lib/upload/interface";
+// @ts-ignore
 import { decrypt } from "iotex-antenna/lib/account/wallet";
+import isElectron from "is-electron";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 import React, { PureComponent } from "react";
+import { xconf } from "../common/xconf";
 import { PasswordFormInputItem } from "./contract/cards";
 import { getAntenna } from "./get-antenna";
 import { FormItemLabel } from "./wallet";
-
-function getBase64(file: RcFile, done: Function): void {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => done(reader.result));
-  reader.readAsDataURL(file);
-}
 
 export interface State {
   priKey: string;
@@ -36,35 +36,25 @@ class UnlockByKeystoreFileInner extends PureComponent<
     isDecrypting: false
   };
 
-  public unlockWallet = async () => {
-    this.props.form.validateFields(async (err, values) => {
-      if (err) {
-        return;
-      }
-      const { password, keystore } = values;
-      if (!password || !keystore) {
-        return;
-      }
+  public unlockWallet = () => {
+    const { form } = this.props;
+    this.setState({ isDecrypting: true });
+    // setTimeout here is to avoid UI stuck while performing decrypting
+    setTimeout(() => {
+      form.validateFields(async (err, values) => {
+        if (err || !values || !values.password || !values.keystore) {
+          this.setState({ isDecrypting: false });
+          return;
+        }
+        const { password, keystore } = values;
 
-      this.setState({ isDecrypting: true });
-
-      getBase64(keystore.file, async (dataUrl: string) => {
         try {
-          const regex = /^data:.+\/(.+);base64,(.*)$/;
-
-          const matches = String(dataUrl).match(regex) || "";
-          const data = matches[2];
-          const keyObj = JSON.parse(
-            Buffer.from(data, "base64").toString("utf8")
-          );
-
+          const keyObj = JSON.parse(keystore);
           const { privateKey } = decrypt(keyObj, password);
-
           const antenna = getAntenna();
           const account = await antenna.iotx.accounts.privateKeyToAccount(
             privateKey
           );
-
           this.props.setWallet(account);
         } catch (e) {
           const msg = String(e);
@@ -85,7 +75,7 @@ class UnlockByKeystoreFileInner extends PureComponent<
           this.setState({ isDecrypting: false });
         }
       });
-    });
+    }, 50);
   };
 
   public render(): JSX.Element {
@@ -97,9 +87,7 @@ class UnlockByKeystoreFileInner extends PureComponent<
         <div style={{ margin: "24px" }} />
         <Form layout="vertical">
           <p>{t("unlock_by_keystore_file.never_upload")}</p>
-
           <Keystore form={form} />
-
           <PasswordFormInputItem form={form} />
 
           <Button
@@ -116,34 +104,157 @@ class UnlockByKeystoreFileInner extends PureComponent<
 }
 
 type KeystoreProps = { form: WrappedFormUtils };
-type KeystoreState = { fileList: Array<RcFile> };
+type KeystoreState = {
+  keystores: { [index: string]: string };
+  keyname: string;
+};
 
 class Keystore extends React.Component<KeystoreProps, KeystoreState> {
-  public state: KeystoreState = {
-    fileList: []
+  constructor(props: KeystoreProps) {
+    super(props);
+    this.state = {
+      keystores: xconf.getConf("keystores", {}),
+      keyname: ""
+    };
+  }
+
+  public readFileStore = (file: RcFile): boolean => {
+    const { keystores } = this.state;
+    const { form } = this.props;
+    const reader = new FileReader();
+    // Safe check for the file size. It should be < 10KB.
+    if (file.size > 10 * 1024) {
+      notification.error({
+        message: t("input.error.keystore.invalid"),
+        duration: 5
+      });
+      return false;
+    }
+    reader.onload = () => {
+      try {
+        const result = `${reader.result}`;
+        if (JSON.parse(result)) {
+          keystores[file.name] = result;
+          // Update keystores list
+          xconf.setConf("keystores", keystores);
+          // Pass to the form
+          form.setFieldsValue({
+            keystore: result
+          });
+          // Update component state
+          this.setState({
+            keyname: file.name,
+            keystores: {
+              ...keystores
+            }
+          });
+        } else {
+          throw new Error(t("input.error.keystore.invalid"));
+        }
+      } catch (e) {
+        notification.error({
+          message: t("input.error.keystore.invalid"),
+          duration: 5
+        });
+      }
+    };
+    reader.readAsText(file);
+    return false;
+  };
+
+  public renderKeystoreMenu(): JSX.Element {
+    const { keystores } = this.state;
+    const keystoresList = Object.keys(keystores);
+    const uploadProps = {
+      beforeUpload: this.readFileStore,
+      showUploadList: false,
+      accept: ".json,application/json,text/json"
+    };
+
+    if (!keystoresList.length || !isElectron()) {
+      return (
+        <Upload {...uploadProps}>
+          <Button>
+            <Icon type="key" /> {t("unlock_by_keystore_file.select_file")}
+          </Button>
+        </Upload>
+      );
+    }
+
+    const menu = (
+      <Menu>
+        {keystoresList.map(name => (
+          <Menu.Item key={name} onClick={() => this.selectKeystore(name)}>
+            <Tag
+              onClose={() => this.deleteKeystore(name)}
+              closable={true}
+              className="keystore-tag"
+            >
+              {name}
+            </Tag>
+          </Menu.Item>
+        ))}
+        <Menu.Item>
+          <Upload {...uploadProps}>
+            <Icon type="key" /> {t("unlock_by_keystore_file.browse_file")}
+          </Upload>
+        </Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <Dropdown overlay={menu}>
+        <Button>
+          {t("unlock_by_keystore_file.select_file")} <Icon type="down" />
+        </Button>
+      </Dropdown>
+    );
+  }
+
+  public selectKeystore(keyname: string): boolean {
+    const keystores = xconf.getConf("keystores", {});
+    if (keystores[keyname]) {
+      this.setState({ keyname });
+      const { form } = this.props;
+      form.setFieldsValue({
+        keystore: this.state.keystores[keyname]
+      });
+    }
+    return true;
+  }
+
+  public deleteKeystore(keyname: string): boolean {
+    const { keystores } = this.state;
+    const newKeystores: { [index: string]: string } = {};
+    Object.keys(keystores).forEach(name => {
+      if (name !== keyname) {
+        newKeystores[name] = keystores[name];
+      }
+    });
+    const newState: KeystoreState = {
+      keystores: newKeystores,
+      keyname: this.state.keyname
+    };
+    if (keyname === this.state.keyname) {
+      newState.keyname = "";
+    }
+    this.setState(newState);
+    // Update keystores list
+    xconf.setConf("keystores", newKeystores);
+    return true;
+  }
+
+  public clearSelected = () => {
+    const { form } = this.props;
+    form.setFieldsValue({
+      keystore: ""
+    });
+    this.setState({ keyname: "" });
   };
 
   public render(): JSX.Element {
     const { form } = this.props;
-    const { fileList } = this.state;
-    const props = {
-      multiple: false,
-      onRemove: () => {
-        this.setState({
-          fileList: []
-        });
-      },
-
-      beforeUpload: (file: RcFile) => {
-        this.setState({
-          fileList: [file]
-        });
-        return false;
-      },
-
-      fileList
-    };
-
+    const { keyname } = this.state;
     return (
       <Form.Item
         label={<FormItemLabel>{t("wallet.input.keystore")}</FormItemLabel>}
@@ -155,13 +266,21 @@ class Keystore extends React.Component<KeystoreProps, KeystoreState> {
               message: t("input.error.keystore.required")
             }
           ]
-        })(
-          <Upload {...props}>
-            <Button>
-              <Icon type="key" /> {t("unlock_by_keystore_file.select_file")}
-            </Button>
-          </Upload>
-        )}
+        })(this.renderKeystoreMenu())}
+        <div>
+          {keyname ? (
+            <Tag
+              onClose={this.clearSelected}
+              closable={true}
+              className="keystore-tag"
+              style={{
+                marginTop: 5
+              }}
+            >
+              <Icon type="file" /> {keyname}
+            </Tag>
+          ) : null}
+        </div>
       </Form.Item>
     );
   }
