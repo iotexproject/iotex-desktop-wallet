38d8addddfca8bd9ece7463d0466b14de6651569
Tian Pan <puncsky@gmail.com>
2019-05-10 14:44:25 -0700

    bump iotex-react-block-producers

--- src/shared/common/apollo-client.ts
+++ src/shared/common/apollo-client.ts
@@ -2,6 +2,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { ApolloLink } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
+import { createWebBpApolloClient } from "iotex-react-block-producers";
 import isBrowser from "is-browser";
 import fetch from "isomorphic-unfetch";
 // @ts-ignore
@@ -27,3 +28,7 @@ export const apolloClient = new ApolloClient({
   link,
   cache: new InMemoryCache().restore(apolloState)
 });
+
+export const webBpApolloClient = createWebBpApolloClient(
+  "https://member.iotex.io/api-gateway/"
+);
