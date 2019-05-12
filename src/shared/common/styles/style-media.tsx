14f431cd9c792cb2da365fcc95a0ef2318daa4b9
805902285@qq.com <805902285@qq.com>
2019-05-12 21:16:46 +0800

    improve footer ui

--- src/shared/common/styles/style-media.tsx
+++ src/shared/common/styles/style-media.tsx
@@ -4,6 +4,7 @@ export const LAP_WIDTH = 768;
 export const media = {
   palm: `@media only screen and (max-width: ${PALM_WIDTH}px)`,
   lap: `@media only screen and (min-width: ${PALM_WIDTH}px) and (max-width: 768px)`,
+  media960: "@media only screen and (max-width: 960px)",
   desk: "@media only screen and (min-width: 769px) and (max-width: 1280px)",
   deskWide: "@media only screen and (min-width: 1281px)"
 };
