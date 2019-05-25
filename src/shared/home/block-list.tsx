1912ef094e96f9f052f9f9c66e7ea9a66a1b79c4
Hung Nguyen <nmhung1210@gmail.com>
2019-05-26 02:38:28 +0700

    Polish block side (#591)

--- src/shared/home/block-list.tsx
+++ src/shared/home/block-list.tsx
@@ -1,12 +1,14 @@
 import { Card, Col, Row } from "antd";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
-import React from "react";
+import React, { useState } from "react";
 import { Query, QueryResult } from "react-apollo";
 
+import { Link } from "react-router-dom";
 import { assetURL } from "../common/asset-url";
 import { FlexLink } from "../common/flex-link";
 import { translateFn } from "../common/from-now";
+import { SpinPreloader } from "../common/spin-preloader";
 import { colors } from "../common/styles/style-color";
 import { GET_BLOCK_METAS, GET_LATEST_HEIGHT } from "../queries";
 
@@ -25,59 +27,82 @@ export const BlockCard = (props: {
 }): JSX.Element => {
   const {
     height = 0,
-    producerAddress = "",
+    producerAddress = "-",
     numActions = 0,
-    timestamp = { seconds: 0, nanos: 0 }
+    timestamp = { seconds: Date.now() / 1000, nanos: 0 }
   } = props.block || {};
+  const [state, setState] = useState({
+    count: 0,
+    height: 0
+  });
+  const rotateY = state.count * 360;
+  if (height !== state.height) {
+    setTimeout(() => {
+      const count =
+        state.count + (document.visibilityState === "hidden" ? 0 : 1);
+      setState({ height, count });
+    }, 100);
+  }
   return (
-    <Card
-      loading={!props.block}
-      style={{
-        marginTop: "0.5rem",
-        width: "100%",
-        backgroundImage: `url(${assetURL(
-          props.index > 0 ? "/block_old.png" : "/block_new.png"
-        )}`,
-        backgroundSize: "cover",
-        color: props.index ? colors.black : colors.white,
-        borderRadius: 8
-      }}
-      bodyStyle={{
-        padding: "1rem 0"
-      }}
-    >
-      <Row type="flex" justify="start" align="middle">
-        <Col
-          style={{
-            width: "0.5rem",
-            height: "0.5rem",
-            backgroundColor: props.index ? colors.white : colors.primary,
-            marginRight: "0.5rem"
-          }}
-        />
-        <Col>{`# ${height}`}</Col>
-      </Row>
-      <div
+    <SpinPreloader spinning={!props.block}>
+      <Card
         style={{
-          padding: "0 1rem",
-          fontSize: "12px",
-          fontWeight: 300,
           marginTop: "0.5rem",
-          lineHeight: 1.8,
-          whiteSpace: "nowrap"
+          background: "transparent",
+          color: props.index ? colors.black : colors.white,
+          borderRadius: 8,
+          transition: "transform 1.8s ease-out",
+          transitionDelay: `${props.index * 0.3}s`,
+          transformStyle: "preserve-3d",
+          transform: `rotateY(${rotateY}deg)`,
+          backgroundSize: "cover",
+          backgroundImage: `url(${assetURL(
+            props.index > 0 ? "/block_old.png" : "/block_new.png"
+          )}`
+        }}
+        bodyStyle={{
+          padding: "1rem 0"
         }}
       >
-        <FlexLink
-          path={`/address/${producerAddress}`}
-          text={`${producerAddress}`.substr(0, 8)}
-        />
-        <div>{`${translateFn(timestamp)}`}</div>
-        <Row type="flex" justify="space-between">
-          <Col>{`${numActions}`}</Col>
-          <Col>{t("home.blocklist.ofActions")}</Col>
-        </Row>
-      </div>
-    </Card>
+        <div
+          style={{
+            opacity: props.block ? 1 : 0
+          }}
+        >
+          <Row type="flex" justify="start" align="middle">
+            <Col
+              style={{
+                width: "0.5rem",
+                height: "0.5rem",
+                backgroundColor: props.index ? colors.white : colors.primary,
+                marginRight: "0.5rem"
+              }}
+            />
+            <Col>{`# ${height}`}</Col>
+          </Row>
+          <div
+            style={{
+              padding: "0 1rem",
+              fontSize: "12px",
+              fontWeight: 300,
+              marginTop: "0.5rem",
+              lineHeight: 1.8,
+              whiteSpace: "nowrap"
+            }}
+          >
+            <FlexLink
+              path={`/address/${producerAddress}`}
+              text={`${producerAddress}`.substr(0, 8)}
+            />
+            <div>{`${translateFn(timestamp)}`}</div>
+            <Row type="flex" justify="space-between">
+              <Col>{`${numActions}`}</Col>
+              <Col>{t("home.blocklist.ofActions")}</Col>
+            </Row>
+          </div>
+        </div>
+      </Card>
+    </SpinPreloader>
   );
 };
 
@@ -100,16 +125,18 @@ export const BlockListByIndex = (props: {
   return (
     <Query
       query={GET_BLOCK_METAS}
+      notifyOnNetworkStatusChange={false}
       variables={{
         byIndex: {
           count,
           start
         }
       }}
+      fetchPolicy="network-only"
+      ssr={false}
     >
       {({
         error,
-        loading,
         data
       }: QueryResult<{
         getBlockMetas: {
@@ -118,7 +145,6 @@ export const BlockListByIndex = (props: {
       }>): JSX.Element | null => {
         if (
           error ||
-          loading ||
           !data ||
           !data.getBlockMetas ||
           !data.getBlockMetas.blkMetas ||
@@ -143,43 +169,85 @@ export const BlockListByIndex = (props: {
 
 export const BlockList = (): JSX.Element => {
   return (
-    <div style={{ marginLeft: "1rem" }}>
-      <Row type="flex" justify="center">
-        {[...Array(4)].map((_, i) => (
-          <Col
-            key={`dot-${i}`}
-            style={{
-              width: 6,
-              height: 6,
-              backgroundColor: colors.black80,
-              borderRadius: 3,
-              margin: "0 6px 2.5rem 0"
-            }}
-          />
-        ))}
-      </Row>
-      <Query query={GET_LATEST_HEIGHT}>
-        {({
-          error,
-          loading,
-          data
-        }: QueryResult<{
-          chainMeta: { height: string };
-        }>): JSX.Element | null => {
-          if (
-            error ||
-            loading ||
-            !data ||
-            !data.chainMeta ||
-            !data.chainMeta.height
-          ) {
-            return <BlockListPlaceHolder count={BLOCK_COUNT} />;
-          }
-          const height = parseInt(`${data.chainMeta.height}`, 10);
-          const start = Math.max(1, height - BLOCK_COUNT);
-          return <BlockListByIndex count={BLOCK_COUNT} start={start} />;
+    <div
+      style={{
+        width: "100%",
+        height: "calc(100vh)",
+        overflow: "hidden"
+      }}
+    >
+      <div
+        style={{
+          height: "calc(100vh - 30px)",
+          marginLeft: "1rem",
+          width: "100%"
         }}
-      </Query>
+        className="no-scrollbar"
+      >
+        <Row type="flex" justify="center">
+          {[...Array(4)].map((_, i) => (
+            <Col
+              key={`dot-${i}`}
+              style={{
+                width: 6,
+                height: 6,
+                backgroundColor: colors.black80,
+                borderRadius: 3,
+                margin: "0 6px 2.5rem 0"
+              }}
+            />
+          ))}
+        </Row>
+        <Query
+          query={GET_LATEST_HEIGHT}
+          pollInterval={10000}
+          fetchPolicy="network-only"
+          ssr={false}
+        >
+          {({
+            error,
+            loading,
+            data
+          }: QueryResult<{
+            chainMeta: { height: string };
+          }>): JSX.Element | null => {
+            if (
+              error ||
+              loading ||
+              !data ||
+              !data.chainMeta ||
+              !data.chainMeta.height
+            ) {
+              return <BlockListPlaceHolder count={BLOCK_COUNT} />;
+            }
+            const height = parseInt(`${data.chainMeta.height}`, 10);
+            const start = Math.max(1, height - BLOCK_COUNT);
+            return <BlockListByIndex count={BLOCK_COUNT} start={start} />;
+          }}
+        </Query>
+        <div style={{ paddingBottom: 30 }} />
+      </div>
+      <div
+        style={{
+          pointerEvents: "none",
+          height: 50,
+          marginTop: -50,
+          position: "relative",
+          marginLeft: "1rem",
+          background:
+            "linear-gradient(to bottom, rgba(240,242,245,0) 0%,rgba(240,242,245,1) 100%)"
+        }}
+      />
+      <div
+        style={{
+          textAlign: "center",
+          marginLeft: "1rem"
+        }}
+      >
+        <Link style={{ color: colors.black95, fontWeight: 300 }} to="/block">
+          {t("block.show_more")}
+        </Link>
+      </div>
     </div>
   );
 };
