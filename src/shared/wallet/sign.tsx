0d2a3dedffabfe68c2e9f6c653e37f2da222732d
Tian Pan <puncsky@gmail.com>
2019-06-02 20:30:23 -0700

    Revert "Revert "add partial sign message""
    This reverts commit a11350970c0b47e91c83f88bed37bcf21d461417.

---
+++ src/shared/wallet/sign.tsx
@@ -0,0 +1,42 @@
+import Button from "antd/lib/button";
+import Form from "antd/lib/form";
+import { WrappedFormUtils } from "antd/lib/form/Form";
+import { get } from "dottie";
+import React, { useState } from "react";
+import { MessageFormInputItem } from "./contract/cards";
+import { getAntenna } from "./get-antenna";
+
+type Props = {
+  form: WrappedFormUtils;
+};
+
+export function SignInner({ form }: Props): JSX.Element {
+  const msgRaw = get(window, "query.message") || "";
+  const [signedMsg, setSignedMsg] = useState("");
+
+  return (
+    <Form>
+      <MessageFormInputItem form={form} initialValue={String(msgRaw)} />
+      {/*
+        // @ts-ignore */}
+      <Button
+        type="primary"
+        onClick={() => {
+          form.validateFields((err, values) => {
+            if (err) {
+              return;
+            }
+            const antenna = getAntenna();
+            // add enumerable
+            const signed = antenna.iotx.accounts.sign(values.message, "");
+            setSignedMsg(signed.toString());
+          });
+        }}
+      />
+
+      {signedMsg && signedMsg}
+    </Form>
+  );
+}
+
+export const Sign = Form.create()(SignInner);
