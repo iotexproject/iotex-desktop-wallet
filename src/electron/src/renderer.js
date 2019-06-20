6f18521c3a13adf9b542628f8a50f7b19cd4fa92
CoderZhi <thecoderzhi@gmail.com>
2019-06-20 15:02:30 -0700

    Wss service (#755)
    * fix encoding problem of signature
    
    * add wss server and service into electron

--- src/electron/src/renderer.js
+++ src/electron/src/renderer.js
@@ -1,11 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const { shell, app, remote } = require("electron");
-const { resolve } = require("path");
+const { shell } = require("electron");
 const { readFileSync, writeFileSync } = require("fs");
+const { getConf, setConf } = require("./config");
 const window = require("global/window");
-const log = require("electron-log");
 const { ipcRenderer } = require("electron");
 const isDev = require("electron-is-dev");
 
@@ -16,39 +15,6 @@ window.xopen = function(url, frameName, features) {
 // The xconf is used to store user configurations with electron.
 // Config's value should be able to store under JSON string.
 window.xconf = new (function() {
-  const userDataPath = (app || remote.app).getPath("userData");
-  const userConfName = resolve(userDataPath, "iotex-wallet-conf.json");
-  let confData = {};
-  try {
-    confData = JSON.parse(readFileSync(userConfName, { encoding: "utf8" }));
-  } catch (e) {
-    log.info("Load empty xconf!");
-  }
-
-  const getConf = function(name, defaultValue) {
-    if (typeof confData[name] !== "undefined") {
-      return confData[name];
-    }
-    return defaultValue;
-  };
-
-  const setConf = function(name, value) {
-    if (typeof name !== "string" || !name) {
-      return false;
-    }
-    if ((typeof value).match(/undefined|function|symbol/i)) {
-      return false;
-    }
-    try {
-      confData[name] = value;
-      writeFileSync(userConfName, JSON.stringify(confData));
-    } catch (e) {
-      log.error(e);
-      return false;
-    }
-    return true;
-  };
-
   return {
     getConf,
     setConf
