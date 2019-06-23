235f9962fd1743006e178af5f2014a79115ebe27
Raullen Chai <raullenchai@gmail.com>
2019-06-22 19:05:50 -0700

    bump CLAIM_GAS_LIMIT to 200K

--- src/erc20/token.ts
+++ src/erc20/token.ts
@@ -19,7 +19,8 @@ const vitaTokens = isBrowser && state.base.vitaTokens;
 
 BigNumber.config({ DECIMAL_PLACES: 6 });
 const regex = /^([0-9]+)I authorize 0x[0-9a-fA-F]{40} to claim in (0x[0-9A-Fa-f]{40})$/;
-export const CLAIM_GAS_LIMIT = "100000";
+// Claim needs ~65000; Claimas needs ~200000
+export const CLAIM_GAS_LIMIT = "200000";
 export const CLAIM_GAS_PRICE = toRau("1", "Qev");
 
 export interface ITokenInfo {
