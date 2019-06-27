cf0403ec5a24f9203cc423e6dcaf856d3b73b462
nmhung1210 <nmhung1210@gmail.com>
2019-06-28 00:58:36 +0900

    Adjust gasLimit by min > 200k

--- src/erc20/erc20.ts
+++ src/erc20/erc20.ts
@@ -35,6 +35,8 @@ export interface DecodeData {
   data: { [key: string]: any };
 }
 
+const MIN_GAS_LIMIT = 200000;
+
 export interface IERC20 {
   address: string;
 
@@ -352,9 +354,14 @@ export class ERC20 implements IERC20 {
     const { gas } = await getAntenna().iotx.estimateGasForAction({
       action: selp.action()
     });
+
+    const gasLimit = new BigNumber(gas).isLessThan(MIN_GAS_LIMIT)
+      ? `${MIN_GAS_LIMIT}`
+      : gas;
+
     return {
       gasPrice: `${gasPrice}`,
-      gasLimit: gas
+      gasLimit
     };
   }
 
