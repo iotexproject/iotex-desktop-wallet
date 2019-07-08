53790304f4456cbc205840d9b8feee9011e00371
Tian Pan <puncsky@gmail.com>
2019-07-08 14:59:26 -0700

    wallet uses ws instead of wss for now fixes 840

--- src/electron/src/server.js
+++ src/electron/src/server.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
-const https = require("https");
+const http = require("http");
 const pem = require("pem");
 const WebSocket = require("ws");
+const console = require("global/console");
 const { getConf, setConf } = require("./config");
 
 function createServerWithCertAndKey(port, cert, key, callback) {
-  const server = https.createServer({ cert, key });
+  const server = http.createServer();
   server.listen(port);
   callback(null, new WebSocket.Server({ server }));
 }
@@ -15,21 +15,21 @@ function createServer(port, callback) {
   if (typeof caStr !== "undefined") {
     try {
       const ca = JSON.parse(caStr);
-      key = ca.serviceKey;
-      cert = ca.certificate;
+      const key = ca.serviceKey;
+      const cert = ca.certificate;
       createServerWithCertAndKey(port, cert, key, callback);
     } catch (err) {
-      log.error("failed to decode certificate");
+      console.error("failed to decode certificate");
     }
   }
   if (typeof cert === "undefined" || typeof key === "undefined") {
     pem.createCertificate({ days: 1, selfSigned: true }, async (err, keys) => {
       if (err) {
-        log.error("failed to create certificate", err);
+        console.error("failed to create certificate", err);
         callback(err, null);
       } else {
-        cert = keys.certificate;
-        key = keys.serviceKey;
+        const cert = keys.certificate;
+        const key = keys.serviceKey;
         setConf("cert", cert);
         setConf("key", key);
         createServerWithCertAndKey(port, cert, key, callback);
