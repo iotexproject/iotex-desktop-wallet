a6b12b48721494506583815cc1ac25ba3da9480c
nmhung1210 <nmhung1210@gmail.com>
2019-07-09 00:25:44 +0700

    Adding type props

--- src/shared/wallet/unlock-by-ledger.tsx
+++ src/shared/wallet/unlock-by-ledger.tsx
@@ -2,9 +2,15 @@ import Button from "antd/lib/button";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 import React from "react";
-import { connect } from "react-redux";
+import { connect, DispatchProp } from "react-redux";
+import { setAccount } from "./wallet-actions";
 
-const UnlockByLedgerComponent = () => {
+interface IUnlockByLedgerComponentProps extends DispatchProp {}
+
+const UnlockByLedgerComponent = (
+  props: IUnlockByLedgerComponentProps
+): JSX.Element => {
+  const { dispatch } = props;
   return (
     <>
       <div style={{ margin: "24px" }} />
@@ -12,6 +18,8 @@ const UnlockByLedgerComponent = () => {
       <Button
         onClick={() => {
           // TODO: Impletement connect ledger device here
+          const account = undefined; // Correct account here
+          dispatch(setAccount(account));
         }}
       >
         {t("unlock-wallet.by_ledger")}
