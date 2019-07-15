8206a8271db72a77f574bd63398f13a3a4acda76
nmhung1210 <nmhung1210@gmail.com>
2019-07-16 06:39:10 +0700

    Update candidates card with data

--- src/shared/home/stats/candidates-card.tsx
+++ src/shared/home/stats/candidates-card.tsx
@@ -1,64 +1,65 @@
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 import React from "react";
+import { Query, QueryResult } from "react-apollo";
+import { webBpApolloClient } from "../../common/apollo-client";
 import { assetURL } from "../../common/asset-url";
 import { colors } from "../../common/styles/style-color";
+import { GET_BP_STATS } from "../../queries";
 import { CompAreaChart } from "../charts/area-chart";
 import { StatsCard } from "./stats-card";
 
-const MockMapData = [
-  {
-    name: "1 Day",
-    value: 500
-  },
-  {
-    name: "2 Day",
-    value: 1300
-  },
-  {
-    name: "3 Day",
-    value: 700
-  },
-  {
-    name: "4 Day",
-    value: 900
-  },
-  {
-    name: "5 Day",
-    value: 600
-  },
-  {
-    name: "6 Day",
-    value: 1200
-  },
-  {
-    name: "7 Day",
-    value: 800
-  }
-];
-
-export interface ICandidatesCardProps {
-  candidates: number;
-  delegates: number;
-}
-export const CandidatesCard = (props: ICandidatesCardProps): JSX.Element => {
+export const CandidatesCard = (): JSX.Element => {
   return (
-    <StatsCard
-      title={t("home.stats.candidates")}
-      titleStyle={{
-        backgroundImage: `url(${assetURL("/icon_overview_Delegates.png")})`
-      }}
-      value={props.candidates}
-      prefix={
-        <div style={{ width: 46, height: 46, marginRight: 30, marginTop: -10 }}>
-          <CompAreaChart
-            data={MockMapData}
-            tinyMode={true}
-            fillColor={colors.warning}
+    <Query query={GET_BP_STATS} client={webBpApolloClient}>
+      {({ data, loading, error }: QueryResult) => {
+        const {
+          bpCandidates = []
+        }: {
+          bpCandidates: Array<{
+            category: "CONSENSUS_DELEGATE" | "DELEGATE_CANDIDATE" | "DELEGATE";
+          }>;
+        } = data || {};
+        const consensusDelegateCount = bpCandidates.filter(
+          a => a.category === "CONSENSUS_DELEGATE"
+        ).length;
+        const candidatesCount = bpCandidates.filter(
+          a => a.category === "DELEGATE_CANDIDATE"
+        ).length;
+        const showLoading = loading || !!error;
+        const {
+          totalCandidatesHistory = []
+        }: { totalCandidatesHistory: Array<{ ts: string; count: number }> } =
+          (data && data.stats) || {};
+        const mapData = totalCandidatesHistory
+          .slice(0, 7)
+          .map(({ ts, count }) => ({
+            name: ts,
+            value: count
+          }));
+        return (
+          <StatsCard
+            title={t("home.stats.candidates")}
+            titleStyle={{
+              backgroundImage: `url(${assetURL(
+                "/icon_overview_Delegates.png"
+              )})`
+            }}
+            value={candidatesCount}
+            loading={showLoading}
+            prefix={
+              <div style={{ width: 46, height: 46 }}>
+                <CompAreaChart
+                  data={mapData}
+                  tinyMode={true}
+                  fillColor={colors.warning}
+                />
+              </div>
+            }
+            suffix={`${consensusDelegateCount}`}
           />
-        </div>
-      }
-      suffix={` / ${props.delegates}`}
-    />
+        );
+      }}
+    </Query>
   );
 };
