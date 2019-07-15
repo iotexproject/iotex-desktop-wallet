585ff0730cd7cb9f90e88a21e924f2f88f5a41f7
nmhung1210 <nmhung1210@gmail.com>
2019-07-16 06:39:54 +0700

    Update votes card with data

--- src/shared/home/stats/votes-card.tsx
+++ src/shared/home/stats/votes-card.tsx
@@ -1,63 +1,58 @@
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 import React from "react";
+import { Query, QueryResult } from "react-apollo";
+import { webBpApolloClient } from "../../common/apollo-client";
 import { assetURL } from "../../common/asset-url";
 import { colors } from "../../common/styles/style-color";
+import { GET_BP_STATS } from "../../queries";
 import { CompAreaChart } from "../charts/area-chart";
 import { StatsCard } from "./stats-card";
 
-const MockMapData = [
-  {
-    name: "1 Day",
-    value: 500
-  },
-  {
-    name: "2 Day",
-    value: 1300
-  },
-  {
-    name: "3 Day",
-    value: 700
-  },
-  {
-    name: "4 Day",
-    value: 900
-  },
-  {
-    name: "5 Day",
-    value: 600
-  },
-  {
-    name: "6 Day",
-    value: 1200
-  },
-  {
-    name: "7 Day",
-    value: 800
-  }
-];
-
-export interface IVotesCardProps {
-  value: number;
-}
-export const VotesCard = (props: IVotesCardProps): JSX.Element => {
+export const VotesCard = (): JSX.Element => {
   return (
-    <StatsCard
-      title={t("home.stats.votes")}
-      titleStyle={{
-        backgroundImage: `url(${assetURL("/icon_overview_Delegates.png")})`
-      }}
-      value={props.value}
-      prefix={
-        <div style={{ width: 46, height: 46, marginRight: 30, marginTop: -10 }}>
-          <CompAreaChart
-            data={MockMapData}
-            tinyMode={true}
-            fillColor={colors.primary}
+    <Query query={GET_BP_STATS} client={webBpApolloClient}>
+      {({ data, loading, error }: QueryResult) => {
+        const {
+          totalVotes = 0,
+          totalVotesHistory = []
+        }: {
+          totalVotes: number;
+          totalVotesHistory: Array<{ ts: string; count: number }>;
+        } = (data && data.stats) || {};
+
+        const showLoading = loading || !!error;
+
+        const mapData = totalVotesHistory.slice(0, 7).map(({ ts, count }) => ({
+          name: ts,
+          value: count
+        }));
+        return (
+          <StatsCard
+            title={t("home.stats.votes")}
+            loading={showLoading}
+            titleStyle={{
+              backgroundImage: `url(${assetURL(
+                "/icon_overview_Delegates.png"
+              )})`
+            }}
+            value={Number(Math.round(totalVotes)).toLocaleString()}
+            prefix={
+              <div style={{ width: 46, height: 46 }}>
+                <CompAreaChart
+                  data={mapData}
+                  tinyMode={true}
+                  fillColor={colors.primary}
+                />
+              </div>
+            }
+            suffix={null}
+            valueStyle={{
+              fontSize: "28px"
+            }}
           />
-        </div>
-      }
-      suffix={null}
-    />
+        );
+      }}
+    </Query>
   );
 };
