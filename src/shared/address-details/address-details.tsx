07a738d926d75390bb26a99f3ed1fd272cfa7a83
Gyan <guptagyan58@gmail.com>
2019-07-17 14:06:52 +0530

    added vita and other token balance in address details

--- src/shared/address-details/address-details.tsx
+++ src/shared/address-details/address-details.tsx
@@ -3,6 +3,7 @@ import Divider from "antd/lib/divider";
 import Icon from "antd/lib/icon";
 // @ts-ignore
 import * as utils from "iotex-antenna/lib/account/utils";
+import isBrowser from "is-browser";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 // @ts-ignore
@@ -10,10 +11,13 @@ import Helmet from "onefx/lib/react-helmet";
 import React, { PureComponent } from "react";
 import { Query, QueryResult } from "react-apollo";
 import { RouteComponentProps, withRouter } from "react-router";
+// @ts-ignore
+import JsonGlobal from "safe-json-globals/get";
 import {
   AccountMeta,
   GetAccountResponse
 } from "../../api-gateway/resolvers/antenna-types";
+import { Token } from "../../erc20/token";
 import { CopyButtonClipboardComponent } from "../common/copy-button-clipboard";
 import { PageTitle } from "../common/page-title";
 import { ShowQrcodeButton } from "../common/show-qrcode-button";
@@ -26,9 +30,39 @@ type PathParamsType = {
   address: string;
 };
 
+type State = {
+  vitaBalance: number;
+  xrc20Balance: number;
+};
+
 type Props = RouteComponentProps<PathParamsType> & {};
 
-class AddressDetailsInner extends PureComponent<Props> {
+class AddressDetailsInner extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      vitaBalance: 0,
+      xrc20Balance: 0
+    };
+  }
+
+  public async componentDidMount(): Promise<void> {
+    const {
+      match: {
+        params: { address }
+      }
+    } = this.props;
+    const defaultERC20Tokens =
+      isBrowser && JsonGlobal("state").base.defaultERC20Tokens;
+    const vitaTokens = isBrowser && JsonGlobal("state").base.vitaTokens;
+    const xrc20Info = await Token.getToken(defaultERC20Tokens[0]).getInfo(
+      address
+    );
+    const vitaInfo = await Token.getToken(vitaTokens[0]).getInfo(address);
+    this.setState({ vitaBalance: vitaInfo.balance.toNumber() });
+    this.setState({ xrc20Balance: xrc20Info.balance.toNumber() });
+  }
+
   public render(): JSX.Element {
     const {
       match: {
@@ -45,9 +79,7 @@ class AddressDetailsInner extends PureComponent<Props> {
             error,
             data
           }: QueryResult<{ getAccount: GetAccountResponse }>) => {
-            if (error) {
-              return null;
-            }
+            if (error) return null;
             if (data && data.getAccount && data.getAccount.accountMeta) {
               addressInfo = data.getAccount.accountMeta;
             }
@@ -78,6 +110,14 @@ class AddressDetailsInner extends PureComponent<Props> {
                         String((addressInfo && addressInfo.balance) || 0),
                         "IOTX"
                       )).toFixed(4)} IOTX`}</div>
+                      <div className={"info"}>{`${(+utils.fromRau(
+                        String(this.state.vitaBalance),
+                        "VITA"
+                      )).toFixed(4)} VITA`}</div>
+                      <div className={"info"}>{`${(+utils.fromRau(
+                        String(this.state.xrc20Balance),
+                        "XRC20"
+                      )).toFixed(4)} XRC20`}</div>
                     </div>
                     <div className={"item"}>
                       <div className={"icon"}>
