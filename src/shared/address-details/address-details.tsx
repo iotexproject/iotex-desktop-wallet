0f06f8e3dcc2a02edb01ed12407f6de91ae44e4e
gyanpgupta <36037560+gyanpgupta@users.noreply.github.com>
2019-07-23 14:17:07 +0530

    added vita and other token balance in address details (#877)
    * added vita and other token balance in address details
    
    * fixed requested changes
    
    * Simplify xrc20 token balances
    
    * Fixed react component keys
    
    * Refresh balance after changed address

--- src/shared/address-details/address-details.tsx
+++ src/shared/address-details/address-details.tsx
@@ -3,6 +3,7 @@ import Divider from "antd/lib/divider";
 import Icon from "antd/lib/icon";
 // @ts-ignore
 import * as utils from "iotex-antenna/lib/account/utils";
+import isBrowser from "is-browser";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 // @ts-ignore
@@ -10,10 +11,13 @@ import Helmet from "onefx/lib/react-helmet";
 import React, { PureComponent } from "react";
 import { Query, QueryResult } from "react-apollo";
 import { RouteComponentProps, withRouter } from "react-router";
+// @ts-ignore
+import JsonGlobal from "safe-json-globals/get";
 import {
   AccountMeta,
   GetAccountResponse
 } from "../../api-gateway/resolvers/antenna-types";
+import { ITokenInfo, Token } from "../../erc20/token";
 import { CopyButtonClipboardComponent } from "../common/copy-button-clipboard";
 import { PageTitle } from "../common/page-title";
 import { ShowQrcodeButton } from "../common/show-qrcode-button";
@@ -26,9 +30,60 @@ type PathParamsType = {
   address: string;
 };
 
+type State = {
+  xrc20Infos: Array<ITokenInfo>;
+  address: string;
+};
+
 type Props = RouteComponentProps<PathParamsType> & {};
 
-class AddressDetailsInner extends PureComponent<Props> {
+class AddressDetailsInner extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      xrc20Infos: [],
+      address: ""
+    };
+  }
+
+  public pollTokenInfos = async (
+    tokens: Array<string>,
+    address: string
+  ): Promise<void> => {
+    const xrc20Infos = await Promise.all(
+      tokens.map(token => Token.getToken(token).getInfo(address))
+    );
+    this.setState({
+      xrc20Infos: xrc20Infos.filter(tokenInfo => !!tokenInfo.symbol),
+      address
+    });
+  };
+
+  public componentDidUpdate(): void {
+    const {
+      match: {
+        params: { address }
+      }
+    } = this.props;
+    if (address === this.state.address) {
+      return;
+    }
+    const xrc20tokens =
+      isBrowser && JsonGlobal("state").base.defaultERC20Tokens;
+    this.pollTokenInfos(xrc20tokens, address);
+  }
+
+  public renderOtherTokenBalance = (): JSX.Element => (
+    <>
+      {this.state.xrc20Infos.map(tokenInfo => (
+        <div
+          key={`token${tokenInfo.symbol}`}
+          className={"info"}
+        >{`${tokenInfo.balanceString} ${tokenInfo.symbol}`}</div>
+      ))}
+    </>
+  );
+
   public render(): JSX.Element {
     const {
       match: {
@@ -78,6 +133,7 @@ class AddressDetailsInner extends PureComponent<Props> {
                         String((addressInfo && addressInfo.balance) || 0),
                         "IOTX"
                       )).toFixed(4)} IOTX`}</div>
+                      {this.renderOtherTokenBalance()}
                     </div>
                     <div className={"item"}>
                       <div className={"icon"}>
