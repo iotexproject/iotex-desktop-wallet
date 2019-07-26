ae3916a834d1f8486550e43aa9c32734a2d69f5c
Gyan <guptagyan58@gmail.com>
2019-07-26 13:40:46 +0530

    added breadcrumb and searchbox(#896)

--- src/shared/common/navigation.tsx
+++ src/shared/common/navigation.tsx
@@ -5,6 +5,7 @@ import withBreadcrumbs, {
 import { NavLink } from "react-router-dom";
 
 const routesConfig = [
+  { path: "/address", breadcrumb: "Address List" },
   { path: "/action", breadcrumb: "Action List" },
   { path: "/action/:hash", breadcrumb: "Action Detail" },
   { path: "/block", breadcrumb: "Block List" },
@@ -16,6 +17,10 @@ const routesConfig = [
 
   // { path: "/block/:id", breadcrumb: `Block# ` }
 ];
+
+const getStyleClass = (index: number) => {
+  return index === 0 ? "collectionLink" : "objectLink";
+};
 
 export const Navigation = ({
   routes
@@ -30,7 +35,7 @@ export const Navigation = ({
       <div style={{ padding: "20px 0", fontSize: "1em" }}>
         {breadcrumbs.map(({ match, breadcrumb }, index) => (
           <span key={match.url}>
-            <NavLink to={match.url}>
+            <NavLink to={match.url} className={getStyleClass(index)}>
               {index !== 0 && ` > `} {breadcrumb}
             </NavLink>
           </span>
