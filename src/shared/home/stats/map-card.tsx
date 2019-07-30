0f9b26b23309e8ae4437870ce772ef6da7b9942b
nmhung1210 <nmhung1210@gmail.com>
2019-07-31 00:27:14 +0700

    Using Query tag that supports ssr to prevent emty state.

--- src/shared/home/stats/map-card.tsx
+++ src/shared/home/stats/map-card.tsx
@@ -1,12 +1,13 @@
-import { Card, Col, Row } from "antd";
+import { Card, Col, Icon, Row, Spin } from "antd";
 import gql from "graphql-tag";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
-import React, { CSSProperties, useState } from "react";
-import { analyticsClient, webBpApolloClient } from "../../common/apollo-client";
+import React, { CSSProperties } from "react";
+import { Query, QueryResult } from "react-apollo";
+import { analyticsClient } from "../../common/apollo-client";
 import { assetURL } from "../../common/asset-url";
 import { colors } from "../../common/styles/style-color";
-import { GET_BP_STATS } from "../../queries";
+import { GET_ANALYTICS_CHAIN } from "../../queries";
 import { CompAreaChart } from "../charts/area-chart";
 
 const fontFamily = "'Heebo',sans-serif,Microsoft YaHei !important";
@@ -70,98 +71,69 @@ export const MapButton = (
   );
 };
 
-export const getActionsData = async (): Promise<{
-  mapdata: Array<{ name: string; value: number }>;
-  currentEpochNumber: number;
-  numberOfActions: number;
-}> => {
-  const {
-    data: {
-      stats: { currentEpochNumber }
-    }
-  } = await webBpApolloClient.query({
-    query: GET_BP_STATS
-  });
-
-  const query = gql`{
-    ${[1, 2, 3, 4, 5, 6, 7].map(day => {
-      return `day${day}:chain{
-        numberOfActions(pagination: { startEpoch: ${currentEpochNumber -
-          day * 24}, epochCount: 24 }) {
-          count
-        }
-      }
-      `;
-    })}
-    chain {
-      numberOfActions{
-        count
-      }
-    }
-  }
-  `;
-  const result: {
-    data: {
-      [index: string]: {
-        numberOfActions: {
-          count: number;
-        };
-      };
-    };
-  } = await analyticsClient.query({ query });
-  const { chain, ...chains } = result.data;
-  const numberOfActions = chain.numberOfActions.count;
-  const mapdata = Object.keys(chains).map((name, i) => ({
-    name: `Day ${i + 1}`,
-    value: result.data[name].numberOfActions.count
-  }));
-  return {
-    mapdata,
-    currentEpochNumber,
-    numberOfActions
-  };
-};
-
-const EMPTY_MAP_DATA: Array<{ name: string; value: number }> = [];
-
 export const MapCard = (): JSX.Element => {
-  const [mapData, setMapData] = useState(EMPTY_MAP_DATA);
-  const [epochNumber, setEpochNumber] = useState(0);
-  getActionsData()
-    .then(({ mapdata, currentEpochNumber }) => {
-      if (currentEpochNumber !== epochNumber) {
-        setMapData(mapdata);
-        setEpochNumber(currentEpochNumber);
-      }
-    })
-    .catch(error => {
-      window.console.error(`failed to getActionsData: ${error}`);
-    });
   return (
-    <Card style={Styles.mapBox} bodyStyle={Styles.mapBoxBody}>
-      <div style={{ padding: 10 }}>
-        {/* <Row type="flex" justify="space-around">
-          <Col span={12}>
-            <MapButton>{t("home.stats.map")}</MapButton>
-          </Col>
-          <Col span={12}>
-            <MapButton>{t("home.stats.energyConsumption")}</MapButton>
-          </Col>
-        </Row> */}
-        <Row type="flex" justify="start">
-          {/* <Col span={12}>
-            <MapButton>{t("home.stats.numberOfAddresses")}</MapButton>
-          </Col> */}
-          <Col span={12}>
-            <MapButton active={true}>
-              {t("home.stats.numberOfTransactions")}
-            </MapButton>
-          </Col>
-        </Row>
-      </div>
-      <div style={Styles.mapContainer}>
-        {mapData.length && <CompAreaChart data={mapData} />}
-      </div>
-    </Card>
+    <Query query={GET_ANALYTICS_CHAIN} client={analyticsClient}>
+      {({ error, loading, data }: QueryResult) => {
+        const showLoading = loading || !!error;
+        const { mostRecentEpoch = 0 } = (data && data.chain) || {};
+        return (
+          <Spin spinning={showLoading} indicator={<Icon type="loading" spin />}>
+            <Card style={Styles.mapBox} bodyStyle={Styles.mapBoxBody}>
+              <div style={{ padding: 10 }}>
+                {/* <Row type="flex" justify="space-around">
+                  <Col span={12}>
+                    <MapButton>{t("home.stats.map")}</MapButton>
+                  </Col>
+                  <Col span={12}>
+                    <MapButton>{t("home.stats.energyConsumption")}</MapButton>
+                  </Col>
+                </Row> */}
+                <Row type="flex" justify="start">
+                  {/* <Col span={12}>
+                    <MapButton>{t("home.stats.numberOfAddresses")}</MapButton>
+                  </Col> */}
+                  <Col span={12}>
+                    <MapButton active={true}>
+                      {t("home.stats.numberOfTransactions")}
+                    </MapButton>
+                  </Col>
+                </Row>
+              </div>
+              <div style={Styles.mapContainer}>
+                {mostRecentEpoch && (
+                  <Query
+                    client={analyticsClient}
+                    query={gql`{
+                    ${[1, 2, 3, 4, 5, 6, 7].map(day => {
+                      return `day${day}:chain{
+                        numberOfActions(pagination: { startEpoch: ${mostRecentEpoch -
+                          day * 24}, epochCount: 24 }) {
+                          count
+                        }
+                      }
+                      `;
+                    })}
+                  }
+                  `}
+                  >
+                    {({ data, error, loading }: QueryResult) => {
+                      if (error || loading || !data) {
+                        return null;
+                      }
+                      const mapdata = Object.keys(data).map((name, i) => ({
+                        name: `Day ${i + 1}`,
+                        value: data[name].numberOfActions.count
+                      }));
+                      return <CompAreaChart data={mapdata} />;
+                    }}
+                  </Query>
+                )}
+              </div>
+            </Card>
+          </Spin>
+        );
+      }}
+    </Query>
   );
 };
