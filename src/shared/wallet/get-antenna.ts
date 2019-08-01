88d5e94239ad1d087fa963d4a262def1eec0438f
Hung Nguyen <nmh@iotex.io>
2019-08-02 03:45:53 +0700

    Fixed #842: [wallet] beautify signing modal and workflow (#919)
    * Polish signing modal
    
    * Fix lint
    
    * Adding flag to enable WsSignerPlugin

--- src/shared/wallet/get-antenna.ts
+++ src/shared/wallet/get-antenna.ts
@@ -1,16 +1,30 @@
 // @ts-ignore
 import window from "global/window";
 import Antenna from "iotex-antenna";
+import { WsSignerPlugin } from "iotex-antenna/lib/plugin/ws";
 import isElectron from "is-electron";
+
+// TODO: enable USE_WS_SIGNER to active the WsSignerPlugin
+const USE_WS_SIGNER = false;
 
 export function getAntenna(): Antenna {
   const injectedWindow: Window & { antenna?: Antenna } = window;
   if (injectedWindow.antenna) {
     return injectedWindow.antenna;
   }
-  const url = isElectron()
-    ? "https://iotexscan.io/iotex-core-proxy"
-    : "/iotex-core-proxy";
-  injectedWindow.antenna = new Antenna(url);
+  if (isElectron()) {
+    injectedWindow.antenna = new Antenna(
+      "https://iotexscan.io/iotex-core-proxy"
+    );
+  } else {
+    injectedWindow.antenna = new Antenna("/iotex-core-proxy", {
+      ...(USE_WS_SIGNER
+        ? {
+            signer: new WsSignerPlugin("ws://localhost:64102/")
+          }
+        : {})
+    });
+  }
+
   return injectedWindow.antenna;
 }
