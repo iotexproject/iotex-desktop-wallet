30c3fba928ed98a236fd7bdad07c1d85de18690d
Gyan <guptagyan58@gmail.com>
2019-08-05 15:55:20 +0530

    fixed search landing page issue(#927)

--- src/shared/home/search-box.tsx
+++ src/shared/home/search-box.tsx
@@ -1,14 +1,9 @@
 import { Input } from "antd";
 import { SearchProps } from "antd/lib/input";
-import { get } from "dottie";
-// @ts-ignore
-import window from "global/window";
-import { publicKeyToAddress } from "iotex-antenna/lib/crypto/crypto";
 import React, { Component } from "react";
 import { withApollo, WithApolloClient } from "react-apollo";
 import { RouteComponentProps, withRouter } from "react-router-dom";
-import { GetBlockMetasRequest } from "../../api-gateway/resolvers/antenna-types";
-import { GET_BLOCK_METAS } from "../queries";
+import { handleSearch } from "../common/search-handler";
 
 type SearchBoxProps = SearchProps &
   RouteComponentProps &
@@ -20,11 +15,6 @@ class SearchBoxComponent extends Component<SearchBoxProps> {
   > = React.createRef();
 
   public componentDidMount(): void {
-    const urlParams = new URLSearchParams(window.location.search);
-    const search = urlParams.get("search");
-    if (search) {
-      this.handleSearch(search);
-    }
     if (!this.inputRef.current) {
       return;
     }
@@ -36,58 +26,6 @@ class SearchBoxComponent extends Component<SearchBoxProps> {
     }
   }
 
-  public handleSearch = async (query: string): Promise<void> => {
-    const { history, client } = this.props;
-    const value = query.trim();
-
-    if (value.startsWith("io")) {
-      return history.push(`/address/${value}`);
-    }
-    if (value.length === 130) {
-      return history.push(`/address/${publicKeyToAddress(value)}`);
-    }
-    if (`${parseInt(value, 10)}` === `${value}`) {
-      try {
-        const { data } = await client.query({
-          query: GET_BLOCK_METAS,
-          variables: {
-            byIndex: {
-              start: parseInt(value, 10) || 0,
-              count: 1
-            },
-            ignoreErrorNotification: true
-          } as GetBlockMetasRequest
-        });
-        if (data) {
-          const hash = get<string>(data, "getBlockMetas.blkMetas.0.hash");
-          if (hash) {
-            return history.push(`/block/${hash}`);
-          }
-        }
-      } catch (e) {
-        return history.push("/notfound");
-      }
-    }
-    try {
-      const validBlock = await client.query({
-        query: GET_BLOCK_METAS,
-        variables: {
-          byHash: {
-            blkHash: value
-          },
-          ignoreErrorNotification: true
-        } as GetBlockMetasRequest
-      });
-      if (validBlock) {
-        return history.push(`/block/${value}`);
-      }
-    } catch (error) {
-      // Block not found
-    }
-    // Default route to action page (Ref. #497)
-    return history.push(`/action/${value}`);
-  };
-
   public render(): JSX.Element {
     const { ...searchprops } = this.props;
     return (
@@ -96,7 +34,7 @@ class SearchBoxComponent extends Component<SearchBoxProps> {
           className={"search-box"}
           {...searchprops}
           onSearch={(query: string) => {
-            this.handleSearch(query);
+            handleSearch(this.props, query);
           }}
         />
       </div>
