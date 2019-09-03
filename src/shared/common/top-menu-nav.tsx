319a7f77ef9ca8f2e9cf0719510243bbbddfd7f7
nmhung1210 <nmhung1210@gmail.com>
2019-09-04 00:29:14 +0700

    Refactor top bar

--- src/shared/common/top-menu-nav.tsx
+++ src/shared/common/top-menu-nav.tsx
@@ -1,70 +1,99 @@
-import { Button, Col, Drawer, Icon, Row } from "antd";
+import { Button, Col, Drawer, Dropdown, Icon, Menu, Row } from "antd";
+import { RowProps } from "antd/lib/row";
+// @ts-ignore
+import window from "global/window";
 import { Languages } from "iotex-react-language-dropdown";
 import { LanguageSwitcher } from "iotex-react-language-dropdown/lib/language-switcher";
+import isBrowser from "is-browser";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
-import React, { CSSProperties, useState } from "react";
+// @ts-ignore
+import { styled } from "onefx/lib/styletron-react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// @ts-ignore
+import JsonGlobal from "safe-json-globals/get";
 import { assetURL } from "./asset-url";
 import { colors } from "./styles/style-color";
 import { ContentPadding } from "./styles/style-padding";
 
+const globalState = isBrowser && JsonGlobal("state");
+
+const multiChain: {
+  current: string;
+  chains: [
+    {
+      name: string;
+      url: string;
+    }
+  ];
+} = (isBrowser && globalState.base.multiChain) || {
+  current: "mainnet",
+  chains: [
+    {
+      name: "mainnet",
+      url: "https://iotexscan.io/"
+    },
+    {
+      name: "testnet",
+      url: "https://testnet.iotexscan.io/"
+    }
+  ]
+};
 export interface INavMenuItem {
-  name: string;
   label: string;
   path?: string;
   items?: Array<INavMenuItem>;
 }
 
-const MAIN_MENUS: Array<INavMenuItem> = [
+const MAIN_NAV_MENUS: Array<INavMenuItem> = [
   {
-    name: "dashboard",
-    label: t("topbar.dashboard"),
+    label: "topbar.dashboard",
     items: [
       {
-        name: "dashboard",
-        label: t("topbar.dashboard"),
+        label: "topbar.dashboard",
         path: "/"
       },
       {
-        name: "actions",
-        label: t("topbar.actions"),
+        label: "topbar.actions",
         path: "/action"
       },
       {
-        name: "blocks",
-        label: t("topbar.blocks"),
+        label: "topbar.blocks",
         path: "/block"
       }
     ]
   },
   {
-    name: "tools",
-    label: t("topbar.tools"),
+    label: "topbar.tools",
     items: [
       {
-        name: "explorer_playground",
-        label: t("topbar.analytics_playground"),
-        path: "/api-gateway/"
+        label: "topbar.explorer_playground",
+        path: "/api-gateway"
       },
       {
-        name: "analytics_playground",
-        label: t("topbar.analytics_playground"),
+        label: "topbar.analytics_playground",
         path: "https://analytics.iotexscan.io/"
       }
     ]
   },
   {
-    name: "vote",
-    label: t("topbar.voting"),
+    label: "topbar.voting",
     path: "https://member.iotex.io"
   },
   {
-    name: "wallet",
-    label: t("topbar.wallet"),
+    label: "topbar.wallet",
     path: "/wallet"
   }
 ];
+
+const CHAIN_MENU_ITEM = {
+  label: multiChain.current,
+  items: multiChain.chains.map(({ name, url }) => ({
+    path: url,
+    label: name
+  }))
+};
 
 const IotexLogo = () => (
   <Link to="/">
@@ -79,25 +108,170 @@ const IotexLogo = () => (
   </Link>
 );
 
+const HoverableStyle = {
+  ":hover": {
+    color: `${colors.primary} !important`
+  },
+  cursor: "pointer"
+};
+
+const HoverableLink = styled(Link, HoverableStyle);
+const HoverableA = styled("a", HoverableStyle);
+const HoverableDiv = styled("div", HoverableStyle);
+
 const TopMobileMenu = (): JSX.Element => {
   const [collapsed, setCollapsed] = useState(true);
   const icon = collapsed ? "menu" : "close";
+  const mobileMenus: Array<INavMenuItem> = [...MAIN_NAV_MENUS, CHAIN_MENU_ITEM];
+  if (isBrowser) {
+    window.document.body.style.overflow = collapsed ? "" : "hidden";
+  }
   return (
-    <Row
-      type="flex"
-      align="middle"
-      justify="space-between"
-      style={{ height: 80 }}
-    >
-      <Col>
-        <IotexLogo />
-      </Col>
+    <div>
       <Row
         type="flex"
         align="middle"
-        justify="end"
-        style={{ height: 80, color: colors.white }}
+        justify="space-between"
+        style={{ height: 80 }}
+        onClick={() => !collapsed && setCollapsed(true)}
       >
+        <Col>
+          <IotexLogo />
+        </Col>
+        <Row
+          type="flex"
+          align="middle"
+          justify="end"
+          style={{ height: 80, color: colors.white }}
+        >
+          <Col>
+            <LanguageSwitcher
+              supportedLanguages={[
+                Languages.EN,
+                Languages.ZH_CN,
+                Languages.IT,
+                Languages.DE
+              ]}
+            />
+          </Col>
+          <Col>
+            <Button
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                padding: 0,
+                color: colors.white,
+                fontSize: 30,
+                backgroundColor: "rgba(0,0,0,0)",
+                border: 0
+              }}
+              icon={icon}
+            ></Button>
+          </Col>
+        </Row>
+      </Row>
+      <Row
+        style={{
+          height: collapsed ? 0 : "100vh",
+          backgroundColor: colors.nav01
+        }}
+      >
+        {mobileMenus.map((item, i) => (
+          <Col xs={24} key={`menu-item-${i}`}>
+            <TopMenuItem menu={item} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+const TopMenuItem = ({ menu }: { menu: INavMenuItem }): JSX.Element => {
+  const { path = "", items, label } = menu;
+  const itemStyle = {
+    cursor: "pointer",
+    color: colors.white
+  };
+  if (!items) {
+    if (path.match(/^https?:\/\//i)) {
+      return (
+        <HoverableA
+          style={itemStyle}
+          href={path}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {t(label)}
+        </HoverableA>
+      );
+    }
+    return (
+      <HoverableLink style={itemStyle} to={path}>
+        {t(label)}
+      </HoverableLink>
+    );
+  }
+  const overlayMenu = (
+    <Menu
+      style={{
+        border: 0,
+        borderRadius: 0,
+        paddingTop: 30,
+        backgroundColor: colors.nav01,
+        cursor: "pointer",
+        minWidth: 150
+      }}
+    >
+      {items.map((item, i) => (
+        <Menu.Item key={`${item.label}-${i}`}>
+          <TopMenuItem menu={item} />
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+  return (
+    <Dropdown trigger={["hover"]} overlay={overlayMenu}>
+      <HoverableDiv style={itemStyle}>
+        <span>{t(label)}</span>
+        <Icon type="caret-down" style={{ paddingLeft: 24 }} />
+      </HoverableDiv>
+    </Dropdown>
+  );
+};
+
+const TopWideMenu = (): JSX.Element => {
+  const rowProps: RowProps = {
+    type: "flex",
+    align: "middle",
+    justify: "space-between",
+    style: { height: 80, color: colors.white },
+    gutter: 60
+  };
+  return (
+    <Row {...rowProps}>
+      <Row {...rowProps}>
+        <Col style={{ marginRight: 40 }}>
+          <IotexLogo />
+        </Col>
+        {MAIN_NAV_MENUS.map((item, i) => (
+          <Col>
+            <TopMenuItem menu={item} />
+          </Col>
+        ))}
+      </Row>
+      <Row {...rowProps} gutter={30} justify="end">
+        {multiChain && (
+          <Col>
+            <TopMenuItem
+              menu={{
+                label: multiChain.current,
+                items: multiChain.chains.map(({ name, url }) => ({
+                  path: url,
+                  label: name
+                }))
+              }}
+            />
+          </Col>
+        )}
         <Col>
           <LanguageSwitcher
             supportedLanguages={[
@@ -108,46 +282,7 @@ const TopMobileMenu = (): JSX.Element => {
             ]}
           />
         </Col>
-        <Col>
-          <Button
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              padding: 0,
-              color: colors.white,
-              fontSize: 30,
-              backgroundColor: "rgba(0,0,0,0)",
-              border: 0
-            }}
-            icon={icon}
-          >
-            <Drawer
-              placement="top"
-              closable={false}
-              onClose={() => setCollapsed(true)}
-              visible={!collapsed}
-              className="top-menu-drawer"
-              style={{ backgroundColor: colors.nav02 }}
-              bodyStyle={{ backgroundColor: colors.transparent }}
-            ></Drawer>
-          </Button>
-        </Col>
       </Row>
-    </Row>
-  );
-};
-
-const TopWideMenu = (): JSX.Element => {
-  return (
-    <Row
-      type="flex"
-      align="middle"
-      justify="space-between"
-      style={{ height: 80 }}
-    >
-      <Col>
-        <IotexLogo />
-      </Col>
-      <Col></Col>
     </Row>
   );
 };
