b1ff24822e4196efdc2d450dddf31a52c8458702
nmhung1210 <nmhung1210@gmail.com>
2019-09-23 21:33:59 +0700

    Adding vertical table component

---
+++ src/shared/common/vertical-table.tsx
@@ -0,0 +1,117 @@
+import { Col, Divider, Row } from "antd";
+import { t } from "onefx/lib/iso-i18n";
+import React, { useState } from "react";
+import { LinkButton } from "./buttons";
+
+export interface IVerticalTableKeyPair<T> {
+  key: string;
+  value: T;
+}
+
+export interface IVerticalTableRendererProps<T> {
+  value: T;
+  record?: IVerticalTableKeyPair<T>;
+  index?: number;
+}
+
+export type VerticalTableRender<T> = React.FC<IVerticalTableRendererProps<T>>;
+
+export interface IVerticalTableProps<T>
+  extends TableProps<IVerticalTableKeyPair<T>> {
+  objectSource: { [index: string]: T };
+  maxRowsCount?: number;
+  showMoreRender?(expanded: boolean): JSX.Element;
+  headerRender?(
+    value: string,
+    record: IVerticalTableKeyPair<T>,
+    index: number
+  ): JSX.Element | string;
+  valueRender?: VerticalTableRender<T>;
+  valueRenderMap?: { [index: string]: VerticalTableRender<T> };
+}
+
+// tslint:disable-next-line:no-any
+function VerticalTable(props: IVerticalTableProps<any>): JSX.Element {
+  const {
+    objectSource,
+    className,
+    style,
+    maxRowsCount,
+    showMoreRender,
+    valueRenderMap,
+    valueRender,
+    headerRender
+  } = props;
+  const [expanded, setExpanded] = useState(false);
+
+  const dataSource = Object.keys(objectSource)
+    .map(
+      (key: string): IVerticalTableKeyPair<T> => ({
+        key,
+        value: objectSource[key]
+      })
+    )
+    .slice(0, expanded ? undefined : maxRowsCount);
+  const moreIcon = expanded ? "up" : "down";
+  const showMore = (showMoreRender && showMoreRender(expanded)) || (
+    <LinkButton icon={moreIcon} style={{ padding: "0px 8px" }}>
+      {t(expanded ? "common.show_less" : "common.show_more")}
+    </LinkButton>
+  );
+
+  return (
+    <div className={`vertical-table ${className || ""}`} style={style}>
+      {dataSource.map((record, index) => {
+        const header = headerRender
+          ? headerRender(record.key, record, index)
+          : `${record.key}`;
+        const valRender =
+          (valueRenderMap && valueRenderMap[record.key]) || valueRender || null;
+        const value =
+          (valRender && valRender({ value: record.value, record, index })) ||
+          `${record.value}`;
+        return (
+          <Row key={`vtable-row-${index}`}>
+            {index > 0 && (
+              <Row>
+                <Col xs={24} sm={24} md={0}>
+                  <Divider style={{ padding: 0, margin: 0 }} />
+                </Col>
+              </Row>
+            )}
+            <Row
+              key={`vtable-row-${record.key}-${index}`}
+              type="flex"
+              align="top"
+              justify="start"
+              className="vtable-row"
+            >
+              <Col xs={24} sm={24} md={0}>
+                <strong>{header}</strong>
+              </Col>
+              <Col xs={0} md={6} lg={4} xxl={3}>
+                {header}
+              </Col>
+              <Col xs={24} sm={24} md={18} lg={20} xxl={21}>
+                {value}
+              </Col>
+            </Row>
+          </Row>
+        );
+      })}
+      {maxRowsCount && (
+        <Row
+          role="main"
+          onClick={() => {
+            setExpanded(!expanded);
+          }}
+          style={{ marginTop: 20, cursor: "pointer" }}
+        >
+          {showMore}
+        </Row>
+      )}
+    </div>
+  );
+}
+
+export { VerticalTable };
