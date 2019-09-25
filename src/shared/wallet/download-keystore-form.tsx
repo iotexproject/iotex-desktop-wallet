712912a6aad8c00072aad4c93be69ba32f20da2a
nmhung1210 <nmhung1210@gmail.com>
2019-09-25 21:37:26 +0700

    Implement qrcode modal

--- src/shared/wallet/download-keystore-form.tsx
+++ src/shared/wallet/download-keystore-form.tsx
@@ -1,12 +1,17 @@
 import Button from "antd/lib/button";
+import Col from "antd/lib/col";
 import Form, { FormComponentProps } from "antd/lib/form/Form";
+import Modal from "antd/lib/modal";
+import Row from "antd/lib/row";
 import dateformat from "dateformat";
 import exportFromJSON from "export-from-json";
 import { encrypt } from "iotex-antenna/lib/account/wallet";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
-import * as React from "react";
+import QRCode from "qrcode.react";
 import { Component } from "react";
+import * as React from "react";
+import { CopyButtonClipboardComponent } from "../common/copy-button-clipboard";
 import { PasswordFormInputItem } from "./contract/cards";
 
 function utcNow(): string {
@@ -45,6 +50,33 @@ class DownloadKeystoreFormInner extends Component<Props & FormComponentProps> {
     });
   };
 
+  public showPrivateKey = () => {
+    const { privateKey } = this.props;
+    const content = (
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{ marginLeft: -38 }}
+      >
+        <Col style={{ margin: 10 }}>
+          <QRCode value={privateKey} />
+        </Col>
+        <Col
+          style={{ wordBreak: "break-all", textAlign: "center", padding: 10 }}
+        >
+          {privateKey}{" "}
+          <CopyButtonClipboardComponent icon="copy" text={privateKey} />
+        </Col>
+      </Row>
+    );
+    Modal.warning({
+      title: t("account.save"),
+      content,
+      okText: t("account.claimAs.close")
+    });
+  };
+
   public render(): JSX.Element {
     const { form, simplify = false } = this.props;
     const { exporting } = this.state;
@@ -65,6 +97,16 @@ class DownloadKeystoreFormInner extends Component<Props & FormComponentProps> {
           onClick={this.onClickHanlder}
         >
           {t("new-wallet.download")}
+        </Button>
+
+        <Button
+          loading={exporting}
+          disabled={exporting}
+          onClick={this.showPrivateKey}
+          icon="qrcode"
+          style={{ marginLeft: 10 }}
+        >
+          {t("new-wallet.showPrivateKey")}
         </Button>
       </Form>
     );
