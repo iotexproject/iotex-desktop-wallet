f66e529432876411471bc52698c382ce685e94f6
nmhung1210 <nmhung1210@gmail.com>
2019-10-01 01:43:00 +0700

    Update action list page

--- src/shared/pages/action-list-page.tsx
+++ src/shared/pages/action-list-page.tsx
@@ -1,12 +1,14 @@
-import { Table, Tag } from "antd";
-import { ColumnProps } from "antd/lib/table";
+import Icon from "antd/lib/icon";
+import Table, { ColumnProps } from "antd/lib/table";
+import Tag from "antd/lib/tag";
 import { get } from "dottie";
 import { fromRau } from "iotex-antenna/lib/account/utils";
 import { publicKeyToAddress } from "iotex-antenna/lib/crypto/crypto";
 import { GetChainMetaResponse } from "iotex-antenna/protogen/proto/api/api_pb";
 import { t } from "onefx/lib/iso-i18n";
-import React, { useState } from "react";
+import React from "react";
 import { Query, QueryResult } from "react-apollo";
+import Helmet from "react-helmet";
 import {
   ActionInfo,
   GetActionsResponse
@@ -15,13 +17,13 @@ import { AddressName } from "../common/address-name";
 import { translateFn } from "../common/from-now";
 import { getActionType } from "../common/get-action-type";
 import { PageNav } from "../common/page-nav-bar";
-import { SpinPreloader } from "../common/spin-preloader";
 import { ContentPadding } from "../common/styles/style-padding";
-import { GET_ACTIONS_BY_INDEX, GET_CHAIN_META } from "../queries";
+import { GET_ACTIONS, GET_CHAIN_META } from "../queries";
 import { ActionFeeRenderer } from "../renderer/action-fee-renderer";
 import { ActionHashRenderer } from "../renderer/action-hash-renderer";
+import { Page } from "./page";
 
-const PAGE_SIZE = 15;
+const PAGE_SIZE = 30;
 
 export function getAddress(record: ActionInfo): string {
   const addr: string =
@@ -39,10 +41,13 @@ export function getAddress(record: ActionInfo): string {
   return addr;
 }
 
-const getActionListColumns = (): Array<ColumnProps<ActionInfo>> => [
+const getActionListColumns = (
+  address?: string
+): Array<ColumnProps<ActionInfo>> => [
   {
     title: t("action.hash"),
     dataIndex: "actHash",
+    width: "10vw",
     render: text => <ActionHashRenderer value={text} />
   },
   {
@@ -53,6 +58,7 @@ const getActionListColumns = (): Array<ColumnProps<ActionInfo>> => [
   {
     title: t("action.sender"),
     dataIndex: "sender",
+    width: "10vw",
     render(_: string, record: ActionInfo, __: number): JSX.Element {
       const addr = publicKeyToAddress(String(record.action.senderPubKey));
       return (
@@ -66,12 +72,28 @@ const getActionListColumns = (): Array<ColumnProps<ActionInfo>> => [
     title: t("action.type"),
     dataIndex: "name",
     render: (_: string, record: ActionInfo, __: number): JSX.Element => {
+      if (address) {
+        const sender = publicKeyToAddress(String(record.action.senderPubKey));
+        const receipt = getAddress(record);
+        if (receipt !== "-") {
+          const type =
+            sender === receipt ? "self" : sender === address ? "out" : "in";
+          const tagColors = {
+            in: "green",
+            out: "red",
+            self: "orange"
+          };
+          return <Tag color={tagColors[type]}>{t(`action.types.${type}`)}</Tag>;
+        }
+      }
+
       return <Tag>{getActionType(record)}</Tag>;
     }
   },
   {
     title: t("render.key.to"),
     dataIndex: "to",
+    width: "10vw",
     render: (_: string, record: ActionInfo): JSX.Element | string => {
       const receipt = getAddress(record);
       return receipt !== "-" ? (
@@ -111,85 +133,111 @@ const getActionListColumns = (): Array<ColumnProps<ActionInfo>> => [
   }
 ];
 
+export interface IActionTable {
+  address?: string;
+  numActions: number;
+}
+
+export const ActionTable: React.FC<IActionTable> = ({
+  numActions,
+  address
+}) => {
+  const start = Math.max(numActions - PAGE_SIZE, 1);
+  const count = PAGE_SIZE;
+  const getVariables = (
+    start: number,
+    count: number
+  ): {
+    byIndex?: {
+      start: number;
+      count: number;
+    };
+    byAddr?: {
+      address: string;
+      start: number;
+      count: number;
+    };
+  } => {
+    return address
+      ? {
+          byAddr: {
+            address,
+            start,
+            count
+          }
+        }
+      : {
+          byIndex: { start, count }
+        };
+  };
+  return (
+    <Query
+      query={GET_ACTIONS}
+      variables={getVariables(start, count)}
+      notifyOnNetworkStatusChange={true}
+    >
+      {({
+        data,
+        loading,
+        fetchMore
+      }: QueryResult<{ getActions: GetActionsResponse }>) => {
+        const actions =
+          get<Array<ActionInfo>>(data || {}, "getActions.actionInfo") || [];
+        return (
+          <Table
+            loading={{
+              spinning: loading,
+              indicator: <Icon type="loading" />
+            }}
+            rowKey="actHash"
+            dataSource={actions}
+            columns={getActionListColumns(address)}
+            style={{ width: "100%" }}
+            scroll={{ x: "auto" }}
+            pagination={{
+              pageSize: count,
+              total: numActions,
+              showQuickJumper: true
+            }}
+            onChange={pagination => {
+              const current = pagination.current || 0;
+              const cStart = start - (current - 1) * count;
+              fetchMore({
+                variables: getVariables(cStart, count),
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) {
+                    return prev;
+                  }
+                  return fetchMoreResult;
+                }
+              });
+            }}
+          />
+        );
+      }}
+    </Query>
+  );
+};
+
 const ActionListPage: React.FC = (): JSX.Element => {
-  const [loading, setLoading] = useState(true);
   return (
     <>
+      <Helmet title={`${t("topbar.actions")} - ${t("meta.description")}`} />
       <PageNav items={[t("topbar.actions")]} />
       <ContentPadding style={{ paddingTop: 20, paddingBottom: 60 }}>
-        <div style={{ width: "100%" }}>
-          <SpinPreloader spinning={loading}>
-            <Query query={GET_CHAIN_META}>
-              {({
-                data
-              }: QueryResult<{ chainMetaData: GetChainMetaResponse }>) => {
-                if (!data) {
-                  return null;
-                }
-                const numActions = Number(get(data, "chainMeta.numActions"));
-                const start = Math.max(numActions - PAGE_SIZE, 1);
-                const count = PAGE_SIZE;
-                return (
-                  <Query
-                    query={GET_ACTIONS_BY_INDEX}
-                    variables={{
-                      byIndex: { start, count }
-                    }}
-                  >
-                    {({
-                      data,
-                      fetchMore
-                    }: QueryResult<{ getActions: GetActionsResponse }>) => {
-                      const actions =
-                        get<Array<ActionInfo>>(
-                          data || {},
-                          "getActions.actionInfo"
-                        ) || [];
-                      if (data) {
-                        setLoading(false);
-                      }
-                      return (
-                        <Table
-                          rowKey={"actHash"}
-                          dataSource={actions}
-                          columns={getActionListColumns()}
-                          style={{ width: "100%" }}
-                          scroll={{ x: true }}
-                          pagination={{
-                            pageSize: count,
-                            total: numActions,
-                            showQuickJumper: true
-                          }}
-                          onChange={pagination => {
-                            const current = pagination.current || 0;
-                            const cStart = start - (current - 1) * count;
-                            setLoading(true);
-                            fetchMore({
-                              query: GET_ACTIONS_BY_INDEX,
-                              variables: {
-                                byIndex: {
-                                  start: cStart < 0 ? 0 : cStart,
-                                  count: count
-                                }
-                              },
-                              updateQuery: (prev, { fetchMoreResult }) => {
-                                if (!fetchMoreResult) {
-                                  return prev;
-                                }
-                                setLoading(false);
-                                return fetchMoreResult;
-                              }
-                            });
-                          }}
-                        />
-                      );
-                    }}
-                  </Query>
-                );
-              }}
-            </Query>
-          </SpinPreloader>
-        </div>
+        <Page header={t("topbar.actions")}>
+          <Query query={GET_CHAIN_META}>
+            {({
+              data
+            }: QueryResult<{ chainMetaData: GetChainMetaResponse }>) => {
+              if (!data) {
+                return null;
+              }
+              const numActions = get<number>(data, "chainMeta.numActions");
+              return <ActionTable numActions={numActions} />;
+            }}
+          </Query>
+        </Page>
       </ContentPadding>
     </>
   );
