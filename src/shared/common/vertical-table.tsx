2afc6728eddf2616f6c6f20b53033cbd34c72b7e
nmhung1210 <nmhung1210@gmail.com>
2019-10-01 00:55:16 +0700

    Add loading for vtable

--- src/shared/common/vertical-table.tsx
+++ src/shared/common/vertical-table.tsx
@@ -2,6 +2,7 @@ import { Col, Divider, Row } from "antd";
 import { t } from "onefx/lib/iso-i18n";
 import React, { CSSProperties, useState } from "react";
 import { LinkButton } from "./buttons";
+import { SpinPreloader } from "./spin-preloader";
 
 export interface IVerticalTableKeyPair<T> {
   key: string;
@@ -16,7 +17,8 @@ export interface IVerticalTableRendererProps<T> {
 
 export type VerticalTableRender<T> = React.FC<IVerticalTableRendererProps<T>>;
 
-export interface IVerticalTableProps<T> {
+// tslint:disable-next-line:no-any
+export interface IVerticalTableProps<T = any> {
   objectSource: { [index: string]: T };
   maxRowsCount?: number;
   showMoreRender?(expanded: boolean): JSX.Element;
@@ -30,6 +32,7 @@ export interface IVerticalTableProps<T> {
   valueRenderMap?: { [index: string]: VerticalTableRender<any> };
   style?: CSSProperties;
   className?: string;
+  loading?: boolean;
 }
 
 // tslint:disable-next-line:no-any
@@ -42,7 +45,8 @@ function VerticalTable<T = any>(props: IVerticalTableProps<T>): JSX.Element {
     showMoreRender,
     valueRenderMap,
     valueRender,
-    headerRender
+    headerRender,
+    loading
   } = props;
   const [expanded, setExpanded] = useState(false);
 
@@ -63,55 +67,64 @@ function VerticalTable<T = any>(props: IVerticalTableProps<T>): JSX.Element {
 
   return (
     <div className={`vertical-table ${className || ""}`} style={style}>
-      {dataSource.map((record, index) => {
-        const header = headerRender
-          ? headerRender(record.key, record, index)
-          : `${record.key}`;
-        const valRender =
-          (valueRenderMap && valueRenderMap[record.key]) || valueRender || null;
-        const value =
-          (valRender && valRender({ value: record.value, record, index })) ||
-          `${record.value}`;
-        return (
-          <Row key={`vtable-row-${index}`}>
-            {index > 0 && (
-              <Row>
-                <Col xs={24} sm={24} md={0}>
-                  <Divider style={{ padding: 0, margin: 0 }} />
-                </Col>
+      <SpinPreloader spinning={!!loading}>
+        <div>
+          {dataSource.map((record, index) => {
+            const header = headerRender
+              ? headerRender(record.key, record, index)
+              : `${record.key}`;
+            const valRender =
+              (valueRenderMap && valueRenderMap[record.key]) ||
+              valueRender ||
+              null;
+            const value =
+              record.value === undefined
+                ? ""
+                : (valRender &&
+                    valRender({ value: record.value, record, index })) ||
+                  `${record.value}`;
+            return (
+              <Row key={`vtable-row-${index}`}>
+                {index > 0 && (
+                  <Row>
+                    <Col xs={24} sm={24} md={0}>
+                      <Divider style={{ padding: 0, margin: 0 }} />
+                    </Col>
+                  </Row>
+                )}
+                <Row
+                  key={`vtable-row-${record.key}-${index}`}
+                  type="flex"
+                  align="top"
+                  justify="start"
+                  className="vtable-row"
+                >
+                  <Col xs={24} sm={24} md={0}>
+                    <strong>{header}</strong>
+                  </Col>
+                  <Col xs={0} md={6} lg={4} xxl={3}>
+                    {header}
+                  </Col>
+                  <Col xs={24} sm={24} md={18} lg={20} xxl={21}>
+                    {value}
+                  </Col>
+                </Row>
               </Row>
-            )}
+            );
+          })}
+          {maxRowsCount && (
             <Row
-              key={`vtable-row-${record.key}-${index}`}
-              type="flex"
-              align="top"
-              justify="start"
-              className="vtable-row"
+              role="main"
+              onClick={() => {
+                setExpanded(!expanded);
+              }}
+              style={{ marginTop: 20, cursor: "pointer" }}
             >
-              <Col xs={24} sm={24} md={0}>
-                <strong>{header}</strong>
-              </Col>
-              <Col xs={0} md={6} lg={4} xxl={3}>
-                {header}
-              </Col>
-              <Col xs={24} sm={24} md={18} lg={20} xxl={21}>
-                {value}
-              </Col>
+              {showMore}
             </Row>
-          </Row>
-        );
-      })}
-      {maxRowsCount && (
-        <Row
-          role="main"
-          onClick={() => {
-            setExpanded(!expanded);
-          }}
-          style={{ marginTop: 20, cursor: "pointer" }}
-        >
-          {showMore}
-        </Row>
-      )}
+          )}
+        </div>
+      </SpinPreloader>
     </div>
   );
 }
