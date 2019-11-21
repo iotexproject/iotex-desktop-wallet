d15bc8c4f0677ade33e7081a919cc46b951d6499
Koishi <ncysatnaf@gmail.com>
2019-11-22 05:16:01 +0800

    using png image for logo (#1160)

--- src/shared/common/top-menu-bar.tsx
+++ src/shared/common/top-menu-bar.tsx
@@ -109,7 +109,7 @@ const IotexLogo = () => (
   <Link to="/">
     <img
       alt="iotex-logo"
-      src={assetURL("/logo_explorer.svg")}
+      src={assetURL("/logo_explorer.png")}
       style={{
         height: 38,
         width: "auto"
