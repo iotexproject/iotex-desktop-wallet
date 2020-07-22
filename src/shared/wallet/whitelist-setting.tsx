530b782467ceca31b59abf940e4417f332dd6b1e
Jikun Liao <liaojikundever@gmail.com>
2020-07-22 13:47:40 -0400

    Batch upgrade dependencies by end of July #1407

--- src/shared/wallet/whitelist-setting.tsx
+++ src/shared/wallet/whitelist-setting.tsx
@@ -93,7 +93,7 @@ export class WhitelistSetting extends React.Component<Props, State> {
                 <Checkbox name="origin" disabled={true}>
                   <span>
                     <span style={{ marginRight: 10 }}>
-                      {origin} > {method}
+                      {origin} &gt; {method}
                     </span>
                     <CopyButtonClipboardComponent
                       icon="copy"
