85dee73a6372e161c9475aff68476b163ebf1f12
Hung Le <hunglmtb@gmail.com>
2020-07-29 19:18:39 +0700

    fix regex on safari

--- src/shared/renderer/iotx-value-renderer.tsx
+++ src/shared/renderer/iotx-value-renderer.tsx
@@ -1,15 +1,13 @@
 import Tag from "antd/lib/tag";
 import { fromRau } from "iotex-antenna/lib/account/utils";
 import React from "react";
-import { VerticalTableRender } from "../common/vertical-table";
+import {
+  numberWithCommas,
+  VerticalTableRender
+} from "../common/vertical-table";
 
 const IOTXValueRenderer: VerticalTableRender<string> = ({ value }) => {
-  return (
-    <Tag>{`${fromRau(value, "iotx").replace(
-      /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
-      ","
-    )} IOTX`}</Tag>
-  );
+  return <Tag>{`${numberWithCommas(fromRau(value, "iotx"))} IOTX`}</Tag>;
 };
 
 export { IOTXValueRenderer };
