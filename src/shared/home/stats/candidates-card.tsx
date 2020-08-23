a1ac4d671cfb55296035395d1444af68a010547a
liaojikunwork <liaojikundever@gmail.com>
2020-08-22 23:33:43 -0500

    remove chart in delegate card #1585 (#1586)

--- src/shared/home/stats/candidates-card.tsx
+++ src/shared/home/stats/candidates-card.tsx
@@ -3,8 +3,6 @@ import { t } from "onefx/lib/iso-i18n";
 import React from "react";
 import { QueryResult } from "react-apollo";
 import { assetURL } from "../../common/asset-url";
-import { colors } from "../../common/styles/style-color";
-import { CompAreaChart } from "../charts/area-chart";
 import StatsCard from "./stats-card";
 
 export const CandidatesCard = (props: QueryResult): JSX.Element => {
@@ -22,14 +20,7 @@ export const CandidatesCard = (props: QueryResult): JSX.Element => {
   ).length;
   const candidatesCount = bpCandidates.length;
   const showLoading = loading || !!error;
-  const {
-    totalCandidatesHistory = []
-  }: { totalCandidatesHistory: Array<{ ts: string; count: number }> } =
-    (data && data.stats) || {};
-  const mapData = totalCandidatesHistory.slice(0, 7).map(({ ts, count }) => ({
-    name: ts,
-    value: count
-  }));
+
   return (
     <StatsCard
       title={t("home.stats.candidates")}
@@ -38,15 +29,7 @@ export const CandidatesCard = (props: QueryResult): JSX.Element => {
       }}
       value={consensusDelegateCount}
       loading={showLoading}
-      prefix={
-        <div style={{ width: 46, height: 46 }}>
-          <CompAreaChart
-            data={mapData}
-            tinyMode={true}
-            fillColor={colors.warning}
-          />
-        </div>
-      }
+      prefix={null}
       suffix={`${candidatesCount}`}
     />
   );
