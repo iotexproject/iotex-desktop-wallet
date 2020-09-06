f5a2db6200fa3abd95123cc7a2bfe8c4e9a83e13
ZQ-jhon <350037310@qq.com>
2020-09-06 20:19:17 +0800

    fix: 🐛 adding column on XRC20 table #1599  (#1616)
    * fix: 🐛 adding column for XRC20 table #1577
    
    * fix: remove unused column
    
    * chore: removed unused component
    
    * fix: adding hyperlink for xrc20Table tokenName column
    
    * chore: change dataSource from parsed contract.
    
    * chore: added hyperlink for token name render
    
    Co-authored-by: uuz <ncysatnaf@gmail.com>

--- src/shared/renderer/token-name-renderer.tsx
+++ src/shared/renderer/token-name-renderer.tsx
@@ -1,29 +1,44 @@
-import React from "react";
-
-const TokenNameRenderer = ({
-  name,
-  symbol,
-  logo,
-  style
-}: {
+import React, { useState } from "react";
+import { Token } from "../../erc20/token";
+import { LinkButton } from "../common/buttons";
+interface InputProps {
   name: string;
   symbol?: string;
   logo?: string;
-  style?: React.CSSProperties
-}) => {
-  return (
-    <div style={{ display: "flex", alignItems: "center",...style }}>
-      {logo && (
-        <img
-          src={`/image/token/${logo}`}
-          alt="ico"
-          style={{ width: "13px", height: "13px" }}
-        />
-      )}
-      <span style={{ marginLeft: "2px" }}>{name}</span>
-      {symbol && <span>({symbol})</span>}
-    </div>
-  );
+  style?: React.CSSProperties;
+  contract?: string;
+}
+const TokenNameRenderer = (prop: InputProps) => {
+  const [address, setAddress] = useState("");
+  const getTemplate = () => {
+    return (
+      <div style={{ display: "flex", alignItems: "center", ...prop.style }}>
+        {prop.logo && (
+          <img
+            src={`/image/token/${prop.logo}`}
+            alt="ico"
+            style={{ width: "13px", height: "13px" }}
+          />
+        )}
+        <span style={{ marginLeft: "2px" }}>{name}</span>
+        {prop.symbol && <span>({prop.symbol})</span>}
+      </div>
+    );
+  };
+  if (!!prop.contract) {
+    const token = Token.getToken(prop.contract);
+    token
+      .getBasicTokenInfo()
+      .then(info => {
+        setAddress(info.tokenAddress);
+      })
+      .catch(() => {
+        setAddress("");
+      });
+    return <LinkButton href={address}>{getTemplate()}</LinkButton>;
+  } else {
+    return getTemplate();
+  }
 };
 
 export { TokenNameRenderer };
