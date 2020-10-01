1f15980eed3b6c97fe4da721f8d7ea895817a841
805902285@qq.com <805902285@qq.com>
2020-10-01 16:54:25 +0800

    fix: use dropdown to select token type and show balance

--- src/shared/renderer/account-balance-renderer.tsx
+++ src/shared/renderer/account-balance-renderer.tsx
@@ -1,9 +1,10 @@
 import Tag from "antd/lib/tag";
+import Select from "antd/lib/select";
+import Divider from "antd/lib/divider";
 import BigNumber from "bignumber.js";
 import { fromRau } from "iotex-antenna/lib/account/utils";
-import isBrowser from "is-browser";
 import { Dict } from "onefx/lib/types";
-import React from "react";
+import React, { useState } from "react";
 // @ts-ignore
 import JsonGlobal from "safe-json-globals/get";
 import {
@@ -11,21 +12,99 @@ import {
   VerticalTableRender
 } from "../common/vertical-table";
 import { XRC20TokenBalanceTag } from "../common/xrc20-token";
+import { GetTokenMetadataMap } from "../common/common-metadata";
+import { TokenMetadata } from "../../api-gateway/resolvers/token";
+
+const { Option, OptGroup } = Select;
 
 const AccountBalanceRenderer: VerticalTableRender<Dict> = ({
   value: { address, balance }
 }) => {
   BigNumber.config({ DECIMAL_PLACES: 8, ROUNDING_MODE: BigNumber.ROUND_DOWN });
-  const xrc20tokens =
-    (isBrowser && JsonGlobal("state").base.defaultERC20Tokens) || [];
+  const tokenMetadataMap = GetTokenMetadataMap();
+  const allTokenList = Object.keys(tokenMetadataMap).map(key => {
+    return tokenMetadataMap[key];
+  });
+  const optTokenList: Array<{
+    label: "xrc20" | "xrc721";
+    list: Array<TokenMetadata>;
+  }> = [
+    {
+      label: "xrc20",
+      list: []
+    },
+    {
+      label: "xrc721",
+      list: []
+    }
+  ];
+  allTokenList.forEach(token => {
+    if (token.type === "xrc20") {
+      optTokenList[0].list.push(token);
+    } else if (token.type === "xrc721") {
+      optTokenList[1].list.push(token);
+    }
+  });
+  const getTokenAddr = (value: string) => value.split(".").shift() || "";
+  const [loading, setLoading] = useState(false);
+  const [tokenVal, setTokenVal] = useState(
+    allTokenList[0] && allTokenList[0].logo
+      ? getTokenAddr(allTokenList[0].logo)
+      : ""
+  );
+
+  const handleChange = (value: string) => {
+    setLoading(true);
+    if (value) {
+      setTokenVal(getTokenAddr(value));
+    }
+  };
+
+  const BalanceTag = () => {
+    if (!tokenVal) return <span>-</span>;
+    return (
+      <XRC20TokenBalanceTag
+        contract={tokenVal}
+        address={address}
+        loading={loading}
+        done={() => {
+          setLoading(false);
+        }}
+      />
+    );
+  };
+
   return (
     <>
       <Tag>{`${numberWithCommas(fromRau(balance, "iotx"))} IOTX`}</Tag>
-      {xrc20tokens.map((token: string) => (
-        <span key={`balance-${token}`}>
-          <XRC20TokenBalanceTag contract={token} address={address} />
+      <Divider type="vertical" style={{ height: "1.2em" }} />
+      {allTokenList && allTokenList.length ? (
+        <span style={{ marginLeft: 8 }}>
+          <Select
+            placeholder="Select Token"
+            defaultValue={allTokenList[0].logo}
+            style={{ width: 175, marginRight: "8px" }}
+            onChange={handleChange}
+            size="small"
+          >
+            {optTokenList.map(tokenType => {
+              return tokenType.list.length ? (
+                <OptGroup label={tokenType.label} key={`${tokenType.label}`}>
+                  {tokenType.list.map(item => {
+                    return (
+                      <Option
+                        key={`${item.name}_${item.symbol}`}
+                        value={item.logo}
+                      >{`${item.name}(${item.symbol})`}</Option>
+                    );
+                  })}
+                </OptGroup>
+              ) : null;
+            })}
+          </Select>
+          {BalanceTag()}
         </span>
-      ))}
+      ) : null}
     </>
   );
 };
