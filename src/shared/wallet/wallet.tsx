758d628f5d16750d702b8cfc0d4a2ee09f7cd33d
lmlzc <lmlzc@users.noreply.github.com>
2021-02-18 14:36:13 +0800

    Update wallet.tsx
    fix:1771

--- src/shared/wallet/wallet.tsx
+++ src/shared/wallet/wallet.tsx
@@ -1,9 +1,12 @@
 import Col from "antd/lib/grid/col";
+import Alert from "antd/lib/alert";
 import Row from "antd/lib/grid/row";
 // @ts-ignore
 import window from "global/window";
 import { Account } from "iotex-antenna/lib/account/account";
 import isElectron from "is-electron";
+import { t } from "onefx/lib/iso-i18n";
+
 // @ts-ignore
 import { styled } from "onefx/lib/styletron-react";
 import React from "react";
@@ -92,16 +95,17 @@ class WalletInner extends PureComponent<Props, State> {
   };
 
   public renderNoWallet = () => {
+    console.log('renderNoWallet')
     const { createNew } = this.state;
     const { dispatch } = this.props;
     return createNew ? (
       <NewWallet dispatch={dispatch} />
     ) : (
-      <UnlockWallet
-        setCreateNew={() => this.setState({ createNew: true })}
-        chainId={1}
-      />
-    );
+        <UnlockWallet
+          setCreateNew={() => this.setState({ createNew: true })}
+          chainId={1}
+        />
+      );
   };
 
   public componentDidMount(): void {
@@ -111,6 +115,7 @@ class WalletInner extends PureComponent<Props, State> {
   }
 
   public render(): JSX.Element {
+    console.log('render')
     const { createNew } = this.state;
     const { account } = this.props;
     return (
@@ -122,6 +127,17 @@ class WalletInner extends PureComponent<Props, State> {
             gutter={30}
             style={{ margin: "40px 0px" }}
           >
+            {!isElectron() && (
+              <React.Fragment>
+                <Alert
+                  style={{marginLeft:'15px',marginBottom:'1em',fontSize:'16px'}}
+                  message={t("This tool is for developers only. Please DO NOT use this wallet for accessing your assets. Please use ioPay downloaded from its official website: https://iopay.iotex.io")}
+                  type="error"
+                  closable={true}
+                  showIcon={true}
+                />
+              </React.Fragment>
+            )}
             <Col xs={24} sm={24} md={12} lg={14} xl={14}>
               {account && (
                 <WalletTabs
