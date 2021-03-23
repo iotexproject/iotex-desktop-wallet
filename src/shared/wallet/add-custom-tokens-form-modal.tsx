4297ea917beffe8566faed0ab0ad5a43147941a1
chen-yijiang <chenyijiang3.14@gmail.com>
2021-03-23 17:38:01 +0800

    improve custom token dialog

--- src/shared/wallet/add-custom-tokens-form-modal.tsx
+++ src/shared/wallet/add-custom-tokens-form-modal.tsx
@@ -4,7 +4,7 @@ import Modal from "antd/lib/modal";
 
 // @ts-ignore
 // tslint:disable-next-line:import-blacklist
-import { AutoComplete, Input } from "antd";
+import {AutoComplete, Input, message} from "antd";
 import { t } from "onefx/lib/iso-i18n";
 import React from "react";
 import { GetTokenMetadataMap, TokenMetadata } from "../common/common-metadata";
@@ -20,8 +20,10 @@ export interface IAddCustomTokensFormModalProps {
 class AddCustomTokensFormModal extends React.PureComponent<
   IAddCustomTokensFormModalProps
 > {
-  public state: { confirming: boolean } = {
-    confirming: false
+  public state: { confirming: boolean, metadataList: Array<TokenMetadata>, options: Array<JSX.Element> } = {
+    confirming: false,
+    metadataList: [],
+    options: []
   };
   public handleOk = async () => {
     const { form, onOK } = this.props;
@@ -37,21 +39,38 @@ class AddCustomTokensFormModal extends React.PureComponent<
       this.setState({ confirming: false });
     });
   };
-  public render(): JSX.Element {
-    const { form, onCancel, visible = false } = this.props;
-    const { getFieldDecorator } = form;
-    const metadataList: Array<TokenMetadata> = [];
+
+
+  public filterOptions = (data: string) => {
+    const filteredList = this.state.metadataList.filter((item) => {
+      return item.address.startsWith(data) || item.name.startsWith(data)
+    });
+    const options = this.renderOption(filteredList);
+    this.setState({
+      options: options
+    })
+  };
+
+  public componentDidMount(): void {
+    const list: Array<TokenMetadata> = [];
     const tokenMetadataMap = GetTokenMetadataMap();
     if (tokenMetadataMap) {
       for (const [k, v] of Object.entries(tokenMetadataMap)) {
         if (v.type === "xrc20") {
           v.address = k;
-          metadataList.push(v);
+          list.push(v);
         }
       }
     }
+    const options = this.renderOption(list);
+    this.setState({
+      metadataList: list,
+      options: options
+    })
+  }
 
-    const options = metadataList.map(item => {
+  private renderOption(dataList: Array<TokenMetadata>): Array<JSX.Element> {
+    return dataList.map(item => {
       return (
         <AutoComplete.Option key={item.address}>
           <div
@@ -65,7 +84,7 @@ class AddCustomTokensFormModal extends React.PureComponent<
           >
             <div style={{ width: 180, marginRight: 20 }}>
               <img
-                src={`/image/token/${item.logo}`}
+                src={`https://iotexscan.io/image/token/${item.logo}`}
                 alt="ico"
                 style={{ width: "13px", height: "13px" }}
               />
@@ -79,11 +98,16 @@ class AddCustomTokensFormModal extends React.PureComponent<
           </div>
         </AutoComplete.Option>
       );
-    });
+    })
+  }
+
+  public render(): JSX.Element {
+    const { form, onCancel, visible = false } = this.props;
+    const { getFieldDecorator } = form;
 
     return (
       <Modal
-        title={t("account.token.addCustom")}
+        title={t("account.token.addToken")}
         visible={visible}
         onOk={this.handleOk}
         onCancel={onCancel}
@@ -93,7 +117,7 @@ class AddCustomTokensFormModal extends React.PureComponent<
         }}
       >
         <Form.Item
-          label={t("wallet.input.fromXrc20")}
+          label={t("wallet.input.search")}
           wrapperCol={{
             xs: 24
           }}
@@ -104,8 +128,9 @@ class AddCustomTokensFormModal extends React.PureComponent<
             <AutoComplete
               placeholder="io..."
               style={{ width: "100%" }}
-              dataSource={options}
+              dataSource={this.state.options}
               optionLabelProp="value"
+              onSearch={this.filterOptions}
             >
               <Input
                 style={{ width: "100%", backgroundColor: colors.black10 }}
