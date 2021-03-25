68493d81d23e705b05eb6bee7e079a5ce57b6455
chen-yijiang <80504020+chen-yijiang@users.noreply.github.com>
2021-03-25 17:56:21 +0800

    improve custom token dialog (#1805)
    * improve custom token dialog
    
    * query metadata
    
    * fix label position

--- src/shared/wallet/add-custom-tokens-form-modal.tsx
+++ src/shared/wallet/add-custom-tokens-form-modal.tsx
@@ -4,24 +4,29 @@ import Modal from "antd/lib/modal";
 
 // @ts-ignore
 // tslint:disable-next-line:import-blacklist
-import { AutoComplete, Input } from "antd";
+import {AutoComplete, Input} from "antd";
 import { t } from "onefx/lib/iso-i18n";
 import React from "react";
-import { GetTokenMetadataMap, TokenMetadata } from "../common/common-metadata";
+import {connect, DispatchProp} from "react-redux";
+import { TokenMetadata } from "../common/common-metadata";
 import { rulesMap } from "../common/rules";
 import { colors } from "../common/styles/style-color";
+import {IRPCProvider, IWalletState} from "./wallet-reducer";
 
-export interface IAddCustomTokensFormModalProps {
+export interface IAddCustomTokensFormModalProps extends DispatchProp {
   onOK(tokenAddress: string): void;
   onCancel(): void;
   visible?: boolean;
   form: WrappedFormUtils;
+  network?: IRPCProvider
 }
 class AddCustomTokensFormModal extends React.PureComponent<
   IAddCustomTokensFormModalProps
 > {
-  public state: { confirming: boolean } = {
-    confirming: false
+  public state: { confirming: boolean, metadataList: Array<TokenMetadata>, options: Array<JSX.Element> } = {
+    confirming: false,
+    metadataList: [],
+    options: []
   };
   public handleOk = async () => {
     const { form, onOK } = this.props;
@@ -37,21 +42,49 @@ class AddCustomTokensFormModal extends React.PureComponent<
       this.setState({ confirming: false });
     });
   };
-  public render(): JSX.Element {
-    const { form, onCancel, visible = false } = this.props;
-    const { getFieldDecorator } = form;
-    const metadataList: Array<TokenMetadata> = [];
-    const tokenMetadataMap = GetTokenMetadataMap();
+
+
+  public filterOptions = (data: string) => {
+    const filteredList = this.state.metadataList.filter((item) => {
+      return item.address.startsWith(data) || item.name.startsWith(data)
+    });
+    const options = this.renderOption(filteredList);
+    this.setState({
+      options: options
+    })
+  };
+
+  public componentWillReceiveProps(nextProps: IAddCustomTokensFormModalProps): void {
+    if (this.props.network?.url === nextProps.network?.url) {
+      return
+    }
+    const list: Array<TokenMetadata> = [];
+    const tokenMetadataMap = AddCustomTokensFormModal.getTokenMetadata(nextProps.network);
     if (tokenMetadataMap) {
       for (const [k, v] of Object.entries(tokenMetadataMap)) {
         if (v.type === "xrc20") {
           v.address = k;
-          metadataList.push(v);
+          list.push(v);
         }
       }
     }
+    const options = this.renderOption(list);
+    this.setState({
+      metadataList: list,
+      options: options
+    })
+  }
 
-    const options = metadataList.map(item => {
+  public static getTokenMetadata(network?: IRPCProvider): { [key: string]: TokenMetadata } {
+    if (network?.name === "mainnet") {
+      return require("iotex-token-metadata");
+    } else {
+      return require("iotex-testnet-token-metadata");
+    }
+  }
+
+  private renderOption(dataList: Array<TokenMetadata>): Array<JSX.Element> {
+    return dataList.map(item => {
       return (
         <AutoComplete.Option key={item.address}>
           <div
@@ -65,7 +98,7 @@ class AddCustomTokensFormModal extends React.PureComponent<
           >
             <div style={{ width: 180, marginRight: 20 }}>
               <img
-                src={`/image/token/${item.logo}`}
+                src={`${this.props.network ? this.props.network.url : "https://iotexscan.io/"}image/token/${item.logo}`}
                 alt="ico"
                 style={{ width: "13px", height: "13px" }}
               />
@@ -79,11 +112,16 @@ class AddCustomTokensFormModal extends React.PureComponent<
           </div>
         </AutoComplete.Option>
       );
-    });
+    })
+  }
+
+  public render(): JSX.Element {
+    const { form, onCancel, visible = false } = this.props;
+    const { getFieldDecorator } = form;
 
     return (
       <Modal
-        title={t("account.token.addCustom")}
+        title={t("account.token.addToken")}
         visible={visible}
         onOk={this.handleOk}
         onCancel={onCancel}
@@ -93,7 +131,13 @@ class AddCustomTokensFormModal extends React.PureComponent<
         }}
       >
         <Form.Item
-          label={t("wallet.input.fromXrc20")}
+          label={t("wallet.input.search")}
+          style={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "flex-start",
+            flexDirection: "column"
+          }}
           wrapperCol={{
             xs: 24
           }}
@@ -104,8 +148,9 @@ class AddCustomTokensFormModal extends React.PureComponent<
             <AutoComplete
               placeholder="io..."
               style={{ width: "100%" }}
-              dataSource={options}
+              dataSource={this.state.options}
               optionLabelProp="value"
+              onSearch={this.filterOptions}
             >
               <Input
                 style={{ width: "100%", backgroundColor: colors.black10 }}
@@ -118,7 +163,17 @@ class AddCustomTokensFormModal extends React.PureComponent<
   }
 }
 
+const mapStateToProps = (state: {
+  wallet: IWalletState;
+}): {
+  network?: IRPCProvider;
+} => ({
+  network: (state.wallet || {}).network
+});
+
 const AddCustomRPCFormModalCom = Form.create<IAddCustomTokensFormModalProps>()(
   AddCustomTokensFormModal
 );
-export default AddCustomRPCFormModalCom;
+
+export default connect(mapStateToProps)(AddCustomRPCFormModalCom)
+
