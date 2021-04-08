ec0f03f5d1c1937654fbcba127afd92dbf6d8512
TangMing <tangming0915@gmail.com>
2021-04-08 11:47:16 +0800

    add copy button inxrc721_token-address-rederer (#1822)

--- src/shared/renderer/xrc721-token-address-renderer.tsx
+++ src/shared/renderer/xrc721-token-address-renderer.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { VerticalTableRender } from "../common/vertical-table";
+import { CopyToClipboard } from "../common/copy-to-clipboard";
+import Icon from "antd/lib/icon";
 
 const Xrc721TokenAddressRenderer: VerticalTableRender<string> = ({ value }) => {
   return (
     <span className="ellipsis-text" style={{ maxWidth: "30vw", minWidth: 100 }}>
-      <Link to={`/xrc721-token/${value}`}>{value}</Link>
+      <Link to={`/xrc721-token/${value}`}>{value}</Link>{" "}
+      <CopyToClipboard text={value}>
+        <Icon type="copy" />
+      </CopyToClipboard>
     </span>
   );
 };
