badaca01cc58d6d3395209670d4951dd28b80a15
zongzheng <304389312@qq.com>
2021-07-09 13:29:43 +0800

    add address convert util

--- src/shared/utils/util.ts
+++ src/shared/utils/util.ts
@@ -1,9 +1,24 @@
 import { validateAddress } from "iotex-antenna/lib/account/utils";
-import { fromString } from "iotex-antenna/lib/crypto/address";
+import { fromBytes, fromString } from "iotex-antenna/lib/crypto/address";
+import { useSelector } from "react-redux";
 
-export const convertAddress = (toEthAddress: boolean, address: string): string => {
+export const convertAddress = (toEthAddress: boolean, address: string = ""): string => {
   if (toEthAddress && address.startsWith("io") && validateAddress(address)) {
     return fromString(address).stringEth()
   }
+  if (!toEthAddress && address.startsWith("0x")) {
+     return fromBytes(Buffer.from(String(address).replace(/^0x/, ""), "hex")).string()
+  }
   return address
 };
+
+export const useConvertAddress = () => {
+   const toEthAddress = useSelector((state: {
+    base: {
+      toEthAddress: boolean
+    };
+  }) => state.base.toEthAddress)
+  return (address: string) => {
+    return  convertAddress(toEthAddress, address)
+  }
+}
